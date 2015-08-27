950310c89ae2753424f86baedf4c4d4e762dc35e
pgrinaway <win98pro>
2015-08-27 16:49:17 -0400

    have SystemGenerator make dummy system

--- system_generator.py
+++ system_generator.py
@@ -2,6 +2,8 @@
 Contains base utility class to generate an openmm System
 object from the topology proposal
 """
+import simtk.openmm as openmm
+
 
 class SystemGenerator(object):
     """
@@ -20,8 +22,9 @@ class SystemGenerator(object):
     """
 
     def __init__(self, proposal):
+        self._system = openmm.System()
         pass
 
     @property
     def system(self):
-	pass
+	return self._system
