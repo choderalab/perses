defc4140a91568f0a7bee76a270628792bcff2c3
pgrinaway <pgrinaway@gmail.com>
2015-10-27 13:24:04 -0400

    complete basic proposal engine

--- perses/rjmc/topology_proposal.py
+++ perses/rjmc/topology_proposal.py
@@ -51,7 +51,7 @@ class Transformation(object):
             probabilities, as well as old and new topologies and atom
             mapping
         """
-        return TopologyProposal(app.Topology(), app.Topology(), 0.0, {0: 0}, {'molecule_smiles': 'CC'})
+        return TopologyProposal(current_topology, app.Topology(), 0.0, {0: 0}, {'molecule_smiles': 'CC'})
 
 
 class SmallSetMoleculeTransformation(Transformation):
@@ -85,7 +85,7 @@ class SmallSetMoleculeTransformation(Transformation):
         current_positions : [n,3] ndarray of floats
             The current positions of the system
         current_metadata : dict
-            Additional metadata about the state--in this case, the current smiles molecule
+            Additional metadata about the state--in this case, the index of the molecule
 
         Returns
         -------
@@ -94,10 +94,72 @@ class SmallSetMoleculeTransformation(Transformation):
             probabilities, as well as old and new topologies and atom
             mapping
         """
-        molecule_idx = self._smiles_dict[current_metadata['smiles']]
-        probabilities = self._tanimotos[molecule_idx, :]
+        molecule_idx = current_metadata['molecule_idx']
+        probabilities_forward = self._tanimotos[molecule_idx, :]
+        #choose an index:
+        proposed_idx = np.random.choice(range(self._n_molecules), p=probabilities_forward)
+        probabilities_reverse = self._tanimotos[proposed_idx, :]
+        log_forward = np.log(probabilities_forward[proposed_idx])
+        log_reverse = np.log(probabilities_reverse[molecule_idx])
+        logp = log_reverse - log_forward
+        #get the atom map:
+        new_to_old_atom_map = self._get_atom_map(self._mol_array[molecule_idx], self._mol_array[proposed_idx])
+
+        #now make a topology out of these things:
+        new_topology, positions = self._oemol_to_openmm_system(self._mol_array[proposed_idx], "MOL")
+
+        return TopologyProposal(current_topology, new_topology, logp, new_to_old_atom_map, {'molecule_idx' : proposed_idx})
 
 
+
+    def _get_atom_map(self, current_molecule, proposed_molecule):
+        """
+        Given two molecules, returns the mapping of atoms between them.
+
+        Arguments
+        ---------
+        current_molecule : openeye.oechem.oemol object
+             The current molecule in the sampler
+        proposed_molecule : openeye.oechem.oemol object
+             The proposed new molecule
+
+        Returns
+        -------
+        new_to_old_atom_map : dict
+            Dictionary of {new_idx : old_idx} format.
+        """
+        mcs = oechem.OEMCSSearch(oechem.OEMCSType_Exhaustive)
+        atomexpr = oechem.OEExprOpts_AtomicNumber
+        bondexpr = 0
+        mcs.Init(current_molecule, atomexpr, bondexpr)
+        mcs.SetMCSFunc(oechem.OEMCSMaxBondsCompleteCycles())
+        unique = True
+        matches = mcs.Match(proposed_molecule, unique)
+        match = matches[0]
+        new_to_old_atom_map = {}
+        for matchpair in match.GetAtoms():
+            old_index = matchpair.pattern.GetIdx()
+            new_index = matchpair.target.GetIdx()
+            new_to_old_atom_map[new_index] = old_index
+        return new_to_old_atom_map
+
+    def _oemol_to_openmm_system(self, oemol, molecule_name):
+        """
+        Create an openmm system out of an oemol
+
+        Returns
+        -------
+        system : openmm.System object
+            the system from the molecule
+        positions : [n,3] np.array of floats
+        """
+        openmoltools.openeye.enter_temp_directory()
+        _ , tripos_mol2_filename = openmoltools.openeye.molecule_to_mol2(oemol, tripos_mol2_filename=molecule_name + '.tripos.mol2', conformer=0, residue_name='MOL')
+        gaff_mol2, frcmod = openmoltools.openeye.run_antechamber(molecule_name, tripos_mol2_filename)
+        prmtop_file, inpcrd_file = openmoltools.utils.run_tleap(molecule_name, gaff_mol2, frcmod)
+        prmtop = app.AmberPrmtopFile(prmtop_file)
+        crd = app.AmberInpcrdFile(inpcrd_file)
+        return prmtop.topology, crd.positions
 
     def _smiles_to_oemol(self):
         """
