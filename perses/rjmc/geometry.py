potentially fixed jacobian issue

perses/rjmc/geometry.py
@@ -163,7 +163,8 @@ class FFGeometryEngine(GeometryEngine):
                     atomic_xyz, logp_atomic = self._propose_atomic_position(new_system, atom, bonded_atom, angle_atom, torsion_atom, torsion_parameters, new_positions)
                     new_positions[atom] = atomic_xyz
                 else:
-                    logp_atomic = self._calculate_logp_atomic_position(new_system, new_positions, atom, bonded_atom, angle_atom, torsion_parameters)
+                    logp_atomic = 0.0
+                    logp_atomic = self._calculate_logp_atomic_position(new_system, new_positions, atom, bonded_atom, angle_atom, torsion_atom, torsion_parameters)
                 logp += logp_atomic + logp_selected_torsion
 
         return new_positions, logp
@@ -243,19 +244,33 @@ class FFGeometryEngine(GeometryEngine):
         phi, logp_torsion = self._propose_torsion_angle(torsion_parameters[6], torsion_parameters[4], torsion_parameters[5])
         #convert spherical to cartesian coordinates
         spherical_coordinates = np.asarray([r, theta, phi])
-        atomic_xyz, detJ = self._internal_to_cartesian(bonded_atom, angle_atom, torsion_atom, r, theta, phi, positions)
+        atomic_xyz, detJ = self._autograd_itoc(bonded_atom, angle_atom, torsion_atom, r, theta, phi, positions)
         #accumulate the forward logp with jacobian correction
         logp_atomic_position = (logp_angle + logp_bond + logp_torsion + np.log(detJ))
         logging.debug("Proposed (r, theta, phi) of (%s, %s, %s)" % (str(r), str(theta), str(phi)))
         return atomic_xyz, logp_atomic_position
 
 
-    def _calculate_logp_atomic_position(self, system, positions, atom, bonded_atom, angle_atom, torsion_parameters):
+    def _calculate_logp_atomic_position(self, system, positions, atom, bonded_atom, angle_atom, torsion_atom, torsion_parameters):
         """
         Calculate the log-probability of a given atom's position
 
         Arguments
         ---------
+        system : simtk.openmm.System object
+            The system whose atomic position needs a logp
+        positions : [n, 3] np.ndarray of floats, Quantity nm
+            The positions of the particles in the system
+        atom : int
+            The index of the atom of interest
+        bonded_atom : int
+            The index of the bonded atom
+        angle_atom : int
+            The index of the angle atom
+        torsion_atom : int
+            The index of the torsion atom
+        torsion_parameters : array
+            The parameters of the torsion
 
         Returns
         -------
@@ -263,22 +278,23 @@ class FFGeometryEngine(GeometryEngine):
             logp of position, including det(J) correction
         """
         #convert cartesian coordinates to spherical
+        self._autograd_ctoi(atom, bonded_atom, angle_atom, torsion_atom, positions)
         relative_xyz = positions[atom] - positions[bonded_atom]
-        spherical_coordinates, detJ = self._cartesian_to_spherical(relative_xyz)
+        internal_coordinates, detJ = self._autograd_ctoi(atom, bonded_atom, angle_atom, torsion_atom, positions)
 
         #get bond parameters and calculate the probability of choosing the current positions
         r0, k_eq = self._get_bond_parameters(system, atom, bonded_atom)
         sigma_bond = 1.0/np.sqrt(2.0*k_eq/k_eq.unit)
-        logp_bond = stats.distributions.norm.logpdf(spherical_coordinates[0]/spherical_coordinates[0].unit, r0/r0.unit, sigma_bond)
+        logp_bond = stats.distributions.norm.logpdf(internal_coordinates[0], r0/r0.unit, sigma_bond)
 
         #calculate the probability of choosing this bond angle
         theta0, k_eq = self._get_angle_parameters(system, atom, bonded_atom, angle_atom)
         sigma_angle = 1.0/np.sqrt(2.0*k_eq/k_eq.unit)
-        logp_angle = stats.distributions.norm.logpdf(spherical_coordinates[1]/spherical_coordinates[1].unit, theta0/theta0.unit, sigma_angle)
+        logp_angle = stats.distributions.norm.logpdf(internal_coordinates[1], theta0/theta0.unit, sigma_angle)
 
         #calculate the probabilty of choosing this torsion angle
         torsion_Z, _ = self._torsion_normalizer(torsion_parameters[6], torsion_parameters[4], torsion_parameters[5])
-        logp_torsion = self._torsion_p(torsion_Z,torsion_parameters[6], torsion_parameters[4], torsion_parameters[5], spherical_coordinates[2]/spherical_coordinates[2].unit)
+        logp_torsion = self._torsion_p(torsion_Z,torsion_parameters[6], torsion_parameters[4], torsion_parameters[5], internal_coordinates[2])
 
         logp_atomic_position = (logp_angle + logp_bond + logp_torsion + np.log(detJ))
 
@@ -548,118 +564,127 @@ class FFGeometryEngine(GeometryEngine):
         max_p = np.max(phi_q/Z)
         return Z, max_p
 
-    def _internal_to_cartesian(self, bond_atom, angle_atom, torsion_atom, r, theta, phi, positions):
+
+
+    def _autograd_ctoi(self, atom, bond, angle, torsion, positions):
+        import autograd
+        import autograd.numpy as np
+
+        positions = positions/positions.unit
+        atom_position = positions[atom]
+
+
+        def _cartesian_to_internal(xyz):
+            """
+            Autograd-based jacobian of transformation from cartesian to internal.
+            Returns without units!
+            """
+            a = positions[bond] - xyz
+            b = positions[angle] - positions[bond]
+            #3-4 bond
+            c = positions[angle] - positions[torsion]
+            a_u = a / np.linalg.norm(a)
+            b_u = b / np.linalg.norm(b)
+            c_u = c / np.linalg.norm(c)
+
+            #bond length
+            r = np.linalg.norm(a)
+
+            #bond angle
+            theta = np.arccos(np.dot(-a_u, b_u))
+
+            #torsion angle
+            plane1 = np.cross(a, b)
+            plane2 = np.cross(b, c)
+
+            phi = np.arccos(np.dot(plane1, plane2)/(np.linalg.norm(plane1)*np.linalg.norm(plane2)))
+
+            if np.dot(np.cross(plane1, plane2), b_u) < 0:
+                phi = -phi
+
+            return np.array([r, theta, phi])
+
+        j = autograd.jacobian(_cartesian_to_internal)
+        internal_coords = _cartesian_to_internal(atom_position)
+        jacobian_det = np.linalg.det(j(atom_position))
+        return internal_coords, atom_position
+
+    def _autograd_itoc(atom, bond, angle, torsion, r, theta, phi, positions):
         """
-        Convert the internal coordinates to cartesian via sympy, also produce detJ
+        Autograd based coordinate conversion internal -> cartesian
+
+        Arguments
+        ---------
+        bond : int
+            index of the bonded atom
+        angle : int
+            index of the angle atom
+        torsion : int
+            index of the torsion atom
+        r : float, Quantity nm
+            bond length
+        theta : float, Quantity rad
+            bond angle
+        phi : float, Quantity rad
+            torsion angle
+        positions : [n, 3] np.array Quantity nm
+            positions of the atoms in the molecule
+
+        Returns
+        -------
+        atom_xyz : [1, 3] np.array Quantity nm
+            The atomic positions in cartesian space
+        detJ : float
+            The absolute value of the determinant of the jacobian
         """
-        N = vector.ReferenceFrame('N')
-        bond_position = positions[bond_atom]
-        angle_position = positions[angle_atom]
-        torsion_position = positions[torsion_atom]
-        r_sym, theta_sym, phi_sym = sympy.symbols('r_sym, theta_sym, phi_sym')
+        import autograd
+        import autograd.numpy as np
+        positions = positions/positions.unit
+        r = r/r.unit
+        theta = theta/theta.unit
+        phi = phi/phi.unit
+        rtp = np.array([r, theta, phi])
 
-        bond_vec = N.x*bond_position[0] + N.y*bond_position[1] + N.z*bond_position[2]
-        angle_vec = N.x*angle_position[0] + N.y*angle_position[1] + N.z*angle_position[2]
-        torsion_vec = N.x*torsion_position[0] + N.y*torsion_position[1] + N.z*torsion_position[2]
+        def _internal_to_cartesian(rthetaphi):
 
-        #2-3 bond
-        a = angle_vec - bond_vec
-        a_u = a.normalize()
+            a = positions[angle] - positions[bond]
+            b = positions[angle] - positions[torsion]
 
-        #3-4 bond
-        b = angle_vec - torsion_vec
-        b_u = b.normalize()
-
-        d = r_sym*a_u
-
-        normal = vector.cross(a, b)
-
-        #rotate the vector about the normal (bond angle)
-        angle_rotation = N.orientnew('angle_rotation', 'Axis',[theta_sym, normal])
-        angle_dcm = N.dcm(angle_rotation)
-        d_theta = vector.dot(angle_dcm, d)
-
-        #rotate the vector about the 2-3 bond (torsion)
-        torsion_rotation = N.orientnew('torsion_rotation', 'Axis', [phi_sym, a])
-        torsion_dcm = N.dcm(torsion_rotation)
-        d_phi = vector.dot(torsion_dcm, d_theta)
-
-        #lambdify the expression and calculate the new coordinates
-        cartesian_atomic_position = sympy.lambdify("r_sym, theta_sym, phi_sym", d_phi, "numpy")(r, theta, phi)
-
-        #now get a jacobian matrix, take the determinant and absolute value, and we're done
-        d_phi_matrix = d_phi.to_matrix(N)
-        d_x = d_phi_matrix[0]
-        d_y = d_phi_matrix[1]
-        d_z = d_phi_matrix[2]
-
-        jacobian_r = [sympy.diff(d_x, 'r_sym'), sympy.diff(d_y, 'r_sym'), sympy.diff(d_z, 'r_sym')]
-        jacobian_theta = [sympy.diff(d_x, 'theta_sym'), sympy.diff(d_y, 'theta_sym'), sympy.diff(d_z, 'theta_sym')]
-        jacobian_phi = [sympy.diff(d_x, 'phi_sym'), sympy.diff(d_y, 'phi_sym'), sympy.diff(d_z, 'phi_sym')]
-        jacobian = sympy.Matrix([jacobian_r, jacobian_theta, jacobian_phi])
-        jacobian_determinant = jacobian.det(method='berkowitz')
-
-        detJ = sympy.lambdify('r_sym, theta_sym, phi_sym', jacobian_determinant)(r, theta, phi)
-
-        return np.array(cartesian_atomic_position), np.abs(detJ)
+            a_u = a / np.linalg.norm(a)
+            b_u = b / np.linalg.norm(b)
 
 
+            d_r = rthetaphi[0]*a_u
 
-    def _cartesian_to_internal(self, atom, bond_atom, angle_atom, torsion_atom, positions):
-        """
-        Use sympy to derive the r, theta, phi for
-        """
-        N = vector.ReferenceFrame('N')
-        Xa, Ya, Za = sympy.symbols("Xa Ya Za")
+            normal = np.cross(a, b)
 
-        atomic_position = positions[atom]
-        bond_position = positions[bond_atom]
-        angle_position = positions[angle_atom]
-        torsion_position = positions[torsion_atom]
-        atom_vec = N.x*Xa+N.y*Ya+N.z*Za
-        bond_vec = N.x*bond_position[0] + N.y*bond_position[1] + N.z*bond_position[2]
-        angle_vec = N.x*angle_position[0] + N.y*angle_position[1] + N.z*angle_position[2]
-        torsion_vec = N.x*torsion_position[0] + N.y*torsion_position[1] + N.z*torsion_position[2]
+            #construct the angle rotation matrix
+            axis_angle = normal / np.linalg.norm(normal)
+            a = np.cos(rthetaphi[1]/2)
+            b, c, d = -axis_angle*np.sin(rthetaphi[1]/2)
+            angle_rotation_matrix =  np.array([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
+                            [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
+                            [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
+            #apply it
+            d_ang = np.dot(angle_rotation_matrix, d_r)
 
-        a = bond_vec - atom_vec
-        b = angle_vec - bond_vec
-        c = torsion_vec - angle_vec
+            #construct the torsion rotation matrix and apply it
+            axis = a_u
+            a = np.cos(rthetaphi[2]/2)
+            b, c, d = -axis*np.sin(rthetaphi[2]/2)
+            torsion_rotation_matrix = np.array([[a*a+b*b-c*c-d*d, 2*(b*c-a*d), 2*(b*d+a*c)],
+                            [2*(b*c+a*d), a*a+c*c-b*b-d*d, 2*(c*d-a*b)],
+                            [2*(b*d-a*c), 2*(c*d+a*b), a*a+d*d-b*b-c*c]])
+            #apply it
+            d_torsion = np.dot(torsion_rotation_matrix, d_ang)
 
-        atom_pos_u = atom_vec.normalize()
-        a_u = a.normalize()
-        b_u = b.normalize()
-        c_u = c.normalize()
+            #add the positions of the bond atom
+            xyz = positions[bond] + d_torsion
 
-        #bond length
-        r_exp = a.magnitude()
+            return xyz
 
-        #bond angle
-        theta_exp = sympy.acos(vector.dot(-a_u, b_u))
-
-        #torsion
-        plane1 = vector.cross(a, b)
-        plane2 = vector.cross(b, c)
-
-        plane1_norm = plane1.magnitude()
-        plane2_norm = plane2.magnitude()
-
-        plane_dot_u = vector.dot(plane1, plane2) / (plane1_norm * plane2_norm)
-
-        phi_exp = sympy.acos(plane_dot_u)
-
-        r = sympy.lambdify("Xa, Ya, Za", r_exp, "numpy")(*atomic_position)
-        theta = sympy.lambdify("Xa, Ya, Za", theta_exp, "numpy")(*atomic_position)
-        phi = sympy.lambdify("Xa, Ya, Za", phi_exp, "numpy")(*atomic_position)
-
-        #calculate the jacobian and its determinant
-        j_rows = []
-        for sym in [r_exp, theta_exp, phi_exp]:
-            j_rows.append([sym.diff(Xa), sym.diff(Ya), sym.diff(Za)])
-        jacobian = sympy.Matrix(j_rows)
-        jacobian_determinant = jacobian.det(method='berkowitz')
-
-        detJ = sympy.lambdify("Xa, Ya, Za",jacobian_determinant, "numpy")(*atomic_position)
-
-
-        return np.array([r, theta, phi]), np.abs(detJ)
+        j = autograd.jacobian(_internal_to_cartesian)
+        atom_xyz = _internal_to_cartesian(rtp)
+        jacobian_det = np.linalg.det(j(rtp))
+        return units.Quantity(atom_xyz, unit=units.nanometers), np.abs(jacobian_det)
 