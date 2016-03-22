fix units and datatypes

perses/rjmc/coordinate_numba.py
@@ -1,6 +1,18 @@
 from numba import jit, float64
 import numpy as np
 
+@jit(float64[:](float64[:], float64[:]), nopython=True, nogil=True, cache=True)
+def _cross_vec3(a, b):
+    c = np.zeros(3)
+    c[0] = a[1]*b[2] - a[2]*b[1]
+    c[1] = a[2]*b[0] - a[0]*b[2]
+    c[2] = a[0]*b[1] - a[1]*b[0]
+    return c
+@jit(float64(float64[:]), nopython=True, nogil=True, cache=True)
+def _norm(a):
+    n_2 = np.dot(a, a)
+    return np.sqrt(n_2)
+
 @jit(float64[:,:](float64[:], float64), nopython=True, nogil=True, cache=True)
 def _rotation_matrix(axis, angle):
     """
@@ -25,18 +37,6 @@ def _rotation_matrix(axis, angle):
                                     cos_angle+axis_squared[2]*(1-cos_angle)])
 
     return rotation_matrix
-
-@jit(float64[:](float64[:], float64[:]), nopython=True, nogil=True, cache=True)
-def _cross_vec3(a, b):
-    c = np.zeros(3)
-    c[0] = a[1]*b[2] - a[2]*b[1]
-    c[1] = a[2]*b[0] - a[0]*b[2]
-    c[2] = a[0]*b[1] - a[1]*b[0]
-    return c
-@jit(float64(float64[:]), nopython=True, nogil=True, cache=True)
-def _norm(a):
-    n_2 = np.dot(a, a)
-    return np.sqrt(n_2)
 
 @jit(float64[:](float64[:], float64[:], float64[:], float64[:]), nopython=True, nogil=True, cache=True)
 def internal_to_cartesian(bond_position, angle_position, torsion_position, internal_coordinates):

perses/rjmc/geometry.py
@@ -7,7 +7,7 @@ import simtk.unit as units
 import logging
 import numpy as np
 import copy
-from perses.rjmc import coordinate_tools
+from perses.rjmc import coordinate_numba
 import simtk.openmm as openmm
 import collections
 import openeye.oechem as oechem
@@ -605,12 +605,12 @@ class FFAllAngleGeometryEngine(GeometryEngine):
         Cartesian to internal function
         """
         #ensure we have the correct units, then remove them
-        atom_position = atom_position.value_in_unit(units.nanometers)
-        bond_position = bond_position.value_in_unit(units.nanometers)
-        angle_position = angle_position.value_in_unit(units.nanometers)
-        torsion_position = torsion_position.value_in_unit(units.nanometers)
+        atom_position = atom_position.value_in_unit(units.nanometers).astype(np.float64)
+        bond_position = bond_position.value_in_unit(units.nanometers).astype(np.float64)
+        angle_position = angle_position.value_in_unit(units.nanometers).astype(np.float64)
+        torsion_position = torsion_position.value_in_unit(units.nanometers).astype(np.float64)
 
-        internal_coords = coordinate_tools._cartesian_to_internal(atom_position, bond_position, angle_position, torsion_position)
+        internal_coords = coordinate_numba.cartesian_to_internal(atom_position, bond_position, angle_position, torsion_position)
 
 
         return internal_coords, internal_coords[0]**2*np.sin(internal_coords[1])
@@ -619,13 +619,13 @@ class FFAllAngleGeometryEngine(GeometryEngine):
         """
         Calculate the cartesian coordinates given the internal, as well as abs(detJ)
         """
-        r = r.in_units_of(units.nanometers)/units.nanometers
-        theta = theta.in_units_of(units.radians)/units.radians
-        phi = phi.in_units_of(units.radians)/units.radians
+        r = r.value_in_unit(units.nanometers)
+        theta = theta.value_in_unit(units.radians)
+        phi = phi.value_in_unit(units.radians)
         bond_position = bond_position.in_units_of(units.nanometers)/units.nanometers
         angle_position = angle_position.in_units_of(units.nanometers)/units.nanometers
         torsion_position = torsion_position.in_units_of(units.nanometers)/units.nanometers
-        xyz = coordinate_tools._internal_to_cartesian(bond_position, angle_position, torsion_position, r, theta, phi)
+        xyz = coordinate_numba.internal_to_cartesian(bond_position, angle_position, torsion_position, np.array([r, theta, phi], dtype=np.float64))
         xyz = units.Quantity(xyz, unit=units.nanometers)
         return xyz, r**2*np.sin(theta)
 
@@ -710,16 +710,18 @@ class FFAllAngleGeometryEngine(GeometryEngine):
         """
         positions_copy = copy.deepcopy(positions)
         positions_copy = positions_copy.in_units_of(units.nanometers)
-        r = r.in_units_of(units.nanometers)
-        theta = theta.in_units_of(units.radians)
+        positions_copy = positions_copy / units.nanometers
+        positions_copy = positions_copy.astype(np.float64)
+        r = r.value_in_unit(units.nanometers)
+        theta = theta.value_in_unit(units.radians)
         bond_atom = torsion.atom2
         angle_atom = torsion.atom3
         torsion_atom = torsion.atom4
-        phis = units.Quantity(np.arange(-np.pi, +np.pi, (2.0*np.pi)/n_divisions), unit=units.radians) # changed to [-pi,+pi) to make it easier to compare with openmm-derived torsions]
-        xyzs = units.Quantity(np.zeros([len(phis), 3]), unit=units.nanometers)
-        for i, phi in enumerate(phis):
-            xyzs[i], _ = self._internal_to_cartesian(positions_copy[bond_atom.idx], positions_copy[angle_atom.idx], positions_copy[torsion_atom.idx], r, theta, phi)
-        return xyzs, phis
+        phis = np.arange(-np.pi, +np.pi, (2.0*np.pi)/n_divisions) # Can't use units here.
+        xyzs = coordinate_numba.torsion_scan(positions_copy[bond_atom.idx], positions_copy[angle_atom.idx], positions_copy[torsion_atom.idx], np.array([r, theta, 0.0]), phis)
+        xyzs_quantity = units.Quantity(xyzs, unit=units.nanometers) #have to put the units back now
+        phis = units.Quantity(phis, unit=units.radians)
+        return xyzs_quantity, phis
 
     def _torsion_log_pmf(self, growth_context, torsion, positions, r, theta, beta, n_divisions=360):
         """

perses/tests/test_geometry_engine.py
@@ -350,10 +350,10 @@ def test_run_geometry_engine(index=0):
     without exceptions. Convert n-pentane to 2-methylpentane
     """
     import copy
-    molecule_name_1 = 'glycine'
-    molecule_name_2 = 'tryptophan'
-    #molecule_name_1 = 'benzene'
-    #molecule_name_2 = 'biphenyl'
+    #molecule_name_1 = 'glycine'
+    #molecule_name_2 = 'tryptophan'
+    molecule_name_1 = 'imatinib'
+    molecule_name_2 = 'erlotinib'
 
     molecule1 = generate_initial_molecule(molecule_name_1)
     molecule2 = generate_initial_molecule(molecule_name_2)
@@ -454,16 +454,6 @@ def test_coordinate_conversion():
         xyz, _ = geometry_engine._internal_to_cartesian(bond_position, angle_position, torsion_position, r, theta, phi)
         assert np.linalg.norm(xyz-atom_position) < 1.0e-12
 
-def test_dihedral_potential():
-    import perses.rjmc.geometry as geometry
-    geometry_engine = geometry.FFAllAngleGeometryEngine({'test': 'true'})
-    molecule_name = 'ethane'
-    molecule2 = generate_initial_molecule(molecule_name)
-    sys, pos, top = oemol_to_openmm_system(molecule2, molecule_name)
-    import perses.rjmc.geometry as geometry
-    geometry_engine = geometry.FFAllAngleGeometryEngine({'test': 'true'})
-    structure = parmed.openmm.load_topology(top, sys)
-
 def test_openmm_dihedral():
     import perses.rjmc.geometry as geometry
     geometry_engine = geometry.FFAllAngleGeometryEngine({'test': 'true'})
@@ -515,7 +505,7 @@ def test_try_random_itoc():
     bond_position = unit.Quantity(np.array([ 0.0765,  0.1  ,  -0.4005]), unit=unit.nanometers)
     angle_position = unit.Quantity(np.array([ 0.0829 , 0.0952 ,-0.2479]) ,unit=unit.nanometers)
     torsion_position = unit.Quantity(np.array([-0.057 ,  0.0951 ,-0.1863] ) ,unit=unit.nanometers)
-    for i in range(10):
+    for i in range(1000):
         atom_position += unit.Quantity(np.random.normal(size=3), unit=unit.nanometers)
         r, theta, phi = _get_internal_from_omm(atom_position, bond_position, angle_position, torsion_position)
         r = (r/r.unit)*unit.nanometers
@@ -714,7 +704,8 @@ def _generate_ffxmls():
 
 if __name__=="__main__":
     #test_coordinate_conversion()
-    #test_run_geometry_engine()
+    for i in range(10):
+        test_run_geometry_engine(index=i)
     #test_existing_coordinates()
     #test_openmm_dihedral()
     #test_try_random_itoc()
@@ -723,4 +714,4 @@ if __name__=="__main__":
     #_tleap_all()
     #test_mutate_from_all_to_all()
     #_generate_ffxmls()
-    test_propose_kinase_inhibitors()
+    #test_propose_kinase_inhibitors()