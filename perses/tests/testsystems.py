d88bf016c79bddb74dd81fd653119f7cdafbe872
Julie Behr <julie.behr@choderalab.org>
2016-03-23 18:15:18 -0400

    add clarity to chain_id

--- perses/tests/testsystems.py
+++ perses/tests/testsystems.py
@@ -164,9 +164,10 @@ class AlanineDipeptideTestSystem(PersesTestSystem):
         from perses.rjmc.topology_proposal import PointMutationEngine
         proposal_metadata = { 'ffxmls' : ['amber99sbildn.xml'] }
         proposal_engines = dict()
+        chain_id = '1'
         allowed_mutations = [[('2','ALA')],[('2','VAL')],[('2','LEU')],[('2','PHE')]]
         for environment in environments:
-            proposal_engines[environment] = PointMutationEngine(system_generators[environment], '1', proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
+            proposal_engines[environment] = PointMutationEngine(system_generators[environment], chain_id, proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
 
         # Generate systems
         systems = dict()
@@ -341,8 +342,9 @@ class T4LysozymeTestSystem(PersesTestSystem):
         from perses.rjmc.topology_proposal import PointMutationEngine
         proposal_metadata = { 'ffxmls' : ['amber99sbildn.xml'] }
         proposal_engines = dict()
+        chain_id = 'A'
         for environment in environments:
-            proposal_engines[environment] = PointMutationEngine(system_generators[environment], 'A', proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
+            proposal_engines[environment] = PointMutationEngine(system_generators[environment], chain_id, proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
 
         # Generate systems
         systems = dict()
@@ -499,8 +501,9 @@ class MybTestSystem(PersesTestSystem):
         from perses.rjmc.topology_proposal import PointMutationEngine
         proposal_metadata = { 'ffxmls' : ['amber99sbildn.xml'] }
         proposal_engines = dict()
+        chain_id
         for environment in environments:
-            proposal_engines[environment] = PointMutationEngine(system_generators[environment], 'B', proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
+            proposal_engines[environment] = PointMutationEngine(system_generators[environment], chain_id, proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
 
         # Generate systems
         systems = dict()
@@ -669,10 +672,11 @@ class AblImatinibTestSystem(PersesTestSystem):
         from perses.rjmc.topology_proposal import PointMutationEngine
         proposal_metadata = { 'ffxmls' : ['amber99sbildn.xml'] }
         proposal_engines = dict()
+        chain_id = 'A'
         for solvent in solvents:
             for component in ['complex', 'receptor']: # Mutations only apply to components that contain the kinase
                 environment = solvent + '-' + component
-                proposal_engines[environment] = PointMutationEngine(system_generators[environment], 'A', proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
+                proposal_engines[environment] = PointMutationEngine(system_generators[environment], chain_id, proposal_metadata=proposal_metadata, allowed_mutations=allowed_mutations)
 
         # Generate systems ror all environments
         systems = dict()
