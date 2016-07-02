3d6e266f29a0ece3c4dfdf69f1c89fcd3c15bcca
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-07-02 16:32:00 -0400

    Update NCMC plotting.

--- perses/analysis/analysis.py
+++ perses/analysis/analysis.py
@@ -84,14 +84,24 @@ class Analysis(object):
                         pass
 
                 if len(work) > 0:
-                    plt.figure(figsize=(6, 6))
+                    plt.figure(figsize=(8, 6))
 
                     for (index, direction) in enumerate(['delete', 'insert']):
                         plt.subplot(2,1,index+1)
-                        plt.plot(work[direction].T, 'k-')
+                        # Plot average work distribution in think solid line
+                        plt.plot(work[direction].mean(0), 'k-', linewidth=1.0, alpha=1.0)
+                        # Plot bundle of work trajectories in transparent lines
+                        plt.plot(work[direction].T, 'k-', linewidth=0.5, alpha=0.3)
+                        # Adjust axes to eliminate large-magnitude outliers (keep 98% of data in-range)
+                        workvals = np.ravel(np.abs(work[direction]))
+                        worklim = np.percentile(workvals, 98)
+                        nsteps = work[direction].shape[1]
+                        plt.axis([0, nsteps, -worklim, +worklim])
+                        # Label plot
                         if index == 2: plt.xlabel('steps')
                         plt.ylabel('work / kT')
                         plt.title(direction)
+                        plt.legend(['average work', 'work trajectories'])
 
                     pdf.savefig()  # saves the current figure into a pdf page
                     plt.close()
