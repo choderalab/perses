769d2324a8b8b949f7e7853e5d36117f23c041f5
jchodera <john.chodera@choderalab.org>
2016-07-13 12:54:57 -0400

    Revert lambda protocols to two-stage electrostatic discharging followed by LJ decoupling.

--- perses/annihilation/ncmc_switching.py
+++ perses/annihilation/ncmc_switching.py
@@ -9,14 +9,10 @@ from perses.storage import NetCDFStorageView
 from perses.tests.utils import quantity_is_finite
 
 default_functions = {
-    #'lambda_sterics' : '2*lambda * step(0.5 - lambda) + (1.0 - step(0.5 - lambda))',
-    #'lambda_electrostatics' : '2*(lambda - 0.5) * step(lambda - 0.5)',
-    'lambda_sterics' : 'lambda',
-    'lambda_electrostatics' : 'lambda',
-    #'lambda_bonds' : '0.9*lambda + 0.1', # don't fully soften bonds
-    #'lambda_angles' : '0.9*lambda + 0.1', # don't fully soften angles
-    'lambda_bonds' : '1',
-    'lambda_angles' : '1',
+    'lambda_sterics' : '2*lambda * step(0.5 - lambda) + (1.0 - step(0.5 - lambda))',
+    'lambda_electrostatics' : '2*(lambda - 0.5) * step(lambda - 0.5)',
+    'lambda_bonds' : '0.9*lambda + 0.1', # don't fully soften bonds
+    'lambda_angles' : '0.9*lambda + 0.1', # don't fully soften angles
     'lambda_torsions' : 'lambda'
     }
 
