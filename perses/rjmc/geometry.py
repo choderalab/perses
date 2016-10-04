Correct problem with units
When unit bearing quantities such as -3 rad are passed in to some
numpy functions (here np.random.uniform), they are apparently
interpreted as 0. This commit strips the units right before the call to
the numpy function.

perses/rjmc/geometry.py
@@ -837,11 +837,13 @@ class FFAllAngleGeometryEngine(GeometryEngine):
             The log probability of the proposal.
         """
         logp_torsions, phis = self._torsion_log_pmf(growth_context, torsion, positions, r, theta, beta, n_divisions=n_divisions)
-        division = 2*np.pi/n_divisions
+        division = units.Quantity(2*np.pi/n_divisions, unit=units.radian)
         phi_median_idx = np.random.choice(range(len(phis)), p=np.exp(logp_torsions))
-        phi = np.random.uniform(phis[phi_median_idx] - division/2.0, phis[phi_median_idx] + division/2.0)
+        phi_min = phis[phi_median_idx] - division/2.0
+        phi_max = phis[phi_median_idx] + division/2.0
+        phi = np.random.uniform(phi_min.value_in_unit(units.radian), phi_max.value_in_unit(units.radian))
         logp = logp_torsions[phi_median_idx] - np.log(2*np.pi / n_divisions) # convert from probability mass function to probability density function so that sum(dphi*p) = 1, with dphi = (2*pi)/n_divisions
-        return phi, logp
+        return units.Quantity(phi, unit=units.radian), logp
 
     def _torsion_logp(self, growth_context, torsion, positions, r, theta, phi, beta, n_divisions=360):
         """