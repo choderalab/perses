5d1475493f78914e77dc989a3b7b5a45d34b978d
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-11-01 01:19:22 -0400

    Fix devtools issue

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -4,7 +4,7 @@ pushd .
 # Install Miniconda
 cd $HOME
 MINICONDA=Miniconda2-latest-Linux-x86_64.sh
-MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
 wget http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
