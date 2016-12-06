ae012e6cec4d1ac4d6a5d6655d1bf90cd8d9ec95
pgrinaway <pgrinaway@gmail.com>
2016-12-06 14:27:17 -0500

    Correct p-value threshold check

--- perses/tests/test_geometry_engine.py
+++ perses/tests/test_geometry_engine.py
@@ -247,7 +247,7 @@ def test_propose_angle():
     #the Kolomogorov-Smirnov test. The null hypothesis is that they are drawn from the same
     #distribution (the test passes).
     (dval, pval) = stats.kstest(angle_array,'norm', args=(theta0_without_units, sigma_without_units))
-    if pval_threshold < 0.05:
+    if pval < pval_threshold:
         raise Exception("The angle may be drawn from the wrong distribution. p= %f" % pval)
 
 def test_propose_bond():
@@ -283,7 +283,7 @@ def test_propose_bond():
     #the Kolomogorov-Smirnov test. The null hypothesis is that they are drawn from the same
     #distribution (the test passes).
     (dval, pval) = stats.kstest(bond_array, 'norm', args=(r0_without_units, sigma_without_units))
-    if pval_threshold < 0.05:
+    if pval < pval_threshold:
         raise Exception("The bond may be drawn from the wrong distribution. p= %f" % pval)
 
 def test_bond_logq():
@@ -651,7 +651,7 @@ def test_propose_torsion():
 
     #now check if the samples match the logp using the Kolmogorov-Smirnov test
     (dval, pval) = stats.kstest(torsion_samples, cdf_func)
-    if pval_threshold < 0.05:
+    if pval < pval_threshold:
         raise Exception("Torsion may not have been drawn from the correct distribution.")
 
 def create_cdf(log_probability_mass_function, phis, n_divisions):
