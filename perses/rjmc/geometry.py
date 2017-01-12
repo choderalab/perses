874c6a97c53b9a285d4e298af02f658060d98b52
pgrinaway <pgrinaway@gmail.com>
2017-01-12 13:17:59 -0500

    Randomize order of atoms for proposals and account for this in the logp

--- perses/rjmc/geometry.py
+++ perses/rjmc/geometry.py
@@ -1921,12 +1921,23 @@ class ProposalOrderTools(object):
                 The log torsion cchoice probability associated with these added torsions.
 
             """
+            from scipy import special
             logp_torsion_choice = 0.0
             while(len(new_atoms))>0:
                 eligible_atoms = self._atoms_eligible_for_proposal(new_atoms, atoms_with_positions)
+
+                #randomize positions
+                eligible_atoms_in_order = np.random.choice(eligible_atoms, size=len(eligible_atoms), replace=False)
+
+                #the logp of this choice is log(1/n!)
+                #gamma is (n-1)!, log-gamma is more numerically stable.
+                logp_torsion_choice += special.gammaln(len(eligible_atoms)+1)
+
                 if (len(new_atoms) > 0) and (len(eligible_atoms) == 0):
                     raise Exception('new_atoms (%s) has remaining atoms to place, but eligible_atoms is empty.' % str(new_atoms))
-                for atom in eligible_atoms:
+
+                #choose the torsions
+                for atom in eligible_atoms_in_order:
                     chosen_torsion, logp_choice = self._choose_torsion(atoms_with_positions, atom)
                     atoms_torsions[atom] = chosen_torsion
                     logp_torsion_choice += logp_choice
