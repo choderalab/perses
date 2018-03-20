ca76356573bd7cd80aed0e6fb122ba316c330cf9
pgrinaway <pgrinaway@gmail.com>
2018-03-20 13:29:38 -0400

    Change relative_setup.py accordingly to only send dictionary of options

--- perses/dispersed/relative_setup.py
+++ perses/dispersed/relative_setup.py
@@ -464,9 +464,9 @@ class NonequilibriumSwitchingFEP(object):
         #Now create the compound states with different alchemical states
         self._hybrid_thermodynamic_states = {0: CompoundThermodynamicState(self._thermodynamic_state, composable_states=[self._hybrid_alchemical_states[0]]), 1: CompoundThermodynamicState(copy.deepcopy(self._thermodynamic_state), composable_states=[self._hybrid_alchemical_states[1]])}
 
-        #create the forward and reverse integrators
-        self._integrators = {0 : AlchemicalNonequilibriumLangevinIntegrator(alchemical_functions=self._forward_functions, nsteps_neq=ncmc_nsteps, temperature=temperature, splitting=self._splitting_string),
-                             1: AlchemicalNonequilibriumLangevinIntegrator(alchemical_functions=self._reverse_functions, nsteps_neq=ncmc_nsteps, temperature=temperature, splitting=self._splitting_string)}
+        #create the forward and reverse integrator options
+        self._integrator_dictionary_options = [{'alchemical_functions' : self._forward_functions, 'nsteps_neq': ncmc_nsteps, 'temperature' : temperature, 'splitting' : self._splitting_string},
+        {'alchemical_functions' : self._reverse_functions, 'nsteps_neq': ncmc_nsteps, 'temperature' : temperature, 'splitting' : self._splitting_string}]
 
         #create the equilibrium MCMove
         self._equilibrium_mc_move = mcmc.LangevinSplittingDynamicsMove(n_steps=n_equil_steps)
@@ -544,7 +544,7 @@ class NonequilibriumSwitchingFEP(object):
             endpoint_perturbation_results_list.append(self._map(feptasks.compute_nonalchemical_perturbation, self._equilibrium_results, hybrid_factory_list, self._nonalchemical_thermodynamic_states.values(), endpoints))
 
             #run a round of nonequilibrium switching:
-            nonequilibrium_results_list.append(self._map(feptasks.run_protocol, self._equilibrium_results, self._hybrid_thermodynamic_states.values(), self._integrators.values(), hybrid_topology_list, write_interval_list, atom_indices_to_save_list, noneq_trajectory_filenames))
+            nonequilibrium_results_list.append(self._map(feptasks.run_protocol, self._equilibrium_results, self._hybrid_thermodynamic_states.values(), self._integrator_dictionary_options, hybrid_topology_list, write_interval_list, atom_indices_to_save_list, noneq_trajectory_filenames))
 
             self._current_iteration +=1
             print(self._current_iteration)
