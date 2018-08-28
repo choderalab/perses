c6da60dadea3084e111b494e6c264a11492c372e
pgrinaway <pgrinaway@gmail.com>
2018-08-28 17:34:23 -0400

    Don't use asterisk

--- perses/samplers/samplers.py
+++ perses/samplers/samplers.py
@@ -444,7 +444,7 @@ class ExpandedEnsembleSampler(object):
         if self.verbose: print("Proposing new topology...")
         [system, topology, positions] = [self.sampler.thermodynamic_state.get_system(remove_thermostat=True), self.topology, self.sampler.sampler_state.positions]
         omm_topology = topology.to_openmm() #convert to OpenMM topology for proposal engine
-        omm_topology.setPeriodicBoxVectors(*self.sampler.sampler_state.box_vectors) #set the box vectors because in OpenMM topology has these...
+        omm_topology.setPeriodicBoxVectors(self.sampler.sampler_state.box_vectors) #set the box vectors because in OpenMM topology has these...
         topology_proposal = self.proposal_engine.propose(system, omm_topology)
         if self.verbose: print("Proposed transformation: %s => %s" % (topology_proposal.old_chemical_state_key, topology_proposal.new_chemical_state_key))
 
