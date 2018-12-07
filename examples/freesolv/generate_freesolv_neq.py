7063293f5d909736d6a77b57b909e50ae4b50b97
Patrick Grinaway <pgrinaway@gmail.com>
2018-12-07 13:19:30 -0500

    Use entire filtered dataset

--- examples/freesolv/generate_freesolv_neq.py
+++ examples/freesolv/generate_freesolv_neq.py
@@ -2,10 +2,8 @@ import numpy as np
 import os
 import yaml
 import itertools
+from openeye import oechem, oeiupac
 import copy
-
-substituted_benzene_names = ['benzene', 'chlorobenzene', 'dichlorobenzene', 'bromobenzene',
-                             'naphthalene', 'ethylbenzene', 'propylbenzene', 'pentylbenzene']
 
 
 def create_yaml_file(mol_a, mol_b, yaml_dict):
@@ -24,10 +22,21 @@ if __name__=="__main__":
     template_script_file_eq = "submit-eq.sh"
     template_script_file_neq = "submit-neq.sh"
 
+    substituted_benzene_smilefile = "filtered_database.smi"
+
+    substituted_benzenes_iupac = []
+
+    istream = oechem.oemolistream(substituted_benzene_smilefile)
+
+    for mol in istream.GetOEMols():
+        mol_copy = oechem.OEMol(mol)
+        substituted_benzenes_iupac.append(oeiupac.OECreateIUPACName(mol_copy))
+
+
     with open("rj_neq_template.yaml", "r") as yamlfile:
         template_yamldict = yaml.load(yamlfile)
 
-    for pair in itertools.permutations(substituted_benzene_names, 2):
+    for pair in itertools.permutations(substituted_benzenes_iupac, 2):
         if pair[0] == pair[1]:
             continue
 
