fixed subtraction of ln(Z_r) and (Z_theta) from log_proposal calculation

perses/rjmc/geometry.py
@@ -281,7 +281,7 @@ class FFAllAngleGeometryEngine(GeometryEngine):
                 bond_k = bond.type.k * self._bond_softening_constant
                 sigma_r = units.sqrt(1/(beta*bond_k))
                 logZ_r = np.log((np.sqrt(2*np.pi)*(sigma_r.value_in_unit(units.angstrom))))
-                logp_r = self._bond_logq(r, bond, beta) - logZ_r
+                logp_r = self._bond_logq(r, bond, beta) #- logZ_r
             else:
                 if direction == 'forward':
                     constraint = self._get_bond_constraint(atom, bond_atom, top_proposal.new_system)
@@ -297,7 +297,7 @@ class FFAllAngleGeometryEngine(GeometryEngine):
             angle_k = angle.type.k * self._angle_softening_constant
             sigma_theta = units.sqrt(1/(beta*angle_k))
             logZ_theta = np.log((np.sqrt(2*np.pi)*(sigma_theta.value_in_unit(units.radians))))
-            logp_theta = self._angle_logq(theta, angle, beta) - logZ_theta
+            logp_theta = self._angle_logq(theta, angle, beta) #- logZ_theta
 
             #propose a torsion angle and calcualate its probability
             if direction=='forward':

perses/tests/test_geometry_engine_analytical.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # In[0]
 import sys
-#sys.path.append('/home/dominic/github/perses/')
 import simtk.openmm as openmm
 import openeye.oechem as oechem
 import openmoltools
@@ -553,8 +552,8 @@ class AnalyticalBeadSystems(object):
         work_comparison=[i+j for i,j in zip(self.work_forward, self.work_reverse)]
         work_forward_var=np.var(self.work_forward)
         work_reverse_var=np.var(self.work_reverse)
-        #print('work_forward: ', self.work_forward)
-        #print('work_reverse: ', self.work_reverse)
+        print('work_forward: ', self.work_forward)
+        print('work_reverse: ', self.work_reverse)
         return work_comparison, work_forward_var, work_reverse_var
 
 