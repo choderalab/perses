d03188db62d00c27eddd31cde2b9fea4e9b6c53c
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2020-03-12 13:21:55 -0400

    improving analysis script

--- perses/analysis/load_simulations.py
+++ perses/analysis/load_simulations.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from openeye import oechem, oegraphsim
+from openmmtools.constants import kB
 
 import logging
 _logger = logging.getLogger()
@@ -16,7 +17,7 @@ class Simulation(object):
 
     This automatically loads the data and performs the data. Results can be accessed through attributes.
 
-    >>> # Load and analyze the simulation for `lig0to1/out.*`
+    >>> # Load and analyze the simulation for `lig0to1/*.nc`
     >>> simulation_results = Simulation(0,1)
     >>> # Report the binding free energy
     >>> print(f'Relative binding free energy: {simulation_results.bindingdg}')
@@ -24,42 +25,38 @@ class Simulation(object):
 
     Parameters
     ----------
-    A : int
-        integer of first ligand
-    B : int
-        integer of second ligand
+    directory : string
+        path to output directory of simulation.
+        this is the `trajectory_directory` variable in
+        a perses simulation yaml file
 
     Attributes
     ----------
-    ligA : int
-        integer of first ligand
-    ligB : int
-        integer of first ligand
     directory : string
         output directory location : lig{A}to{B}
-    vacdg : float
+    _vacdg : float
         relative free energy in vacuum phase (kcal/mol)
-    vacddg : float
+    _vacddg : float
         std error in relative free energy in vacuum phase (kcal/mol)
-    soldg : float
+    _soldg : float
         relative free energy in solvent phase (kcal/mol)
-    solddg : float
+    _solddg : float
         std error in relative free energy in solvent phase (kcal/mol)
-    comdg : float
+    _comdg : float
         relative free energy in complex phase (kcal/mol)
-    comddg : float
+    _comddg : float
         std error in relative free energy in complex phase (kcal/mol)
-    vacdg_history : list(float)
+    _vacdg_history : list(float)
         vacuum free energy at equally spaced intervals of simulation (kcal/mol)
-    soldg_history : type
+    _soldg_history : type
         solvent free energy at equally spaced intervals of simulation (kcal/mol)
-    comdg_history : type
+    _comdg_history : type
         complex free energy at equally spaced intervals of simulation (kcal/mol)
-    vacddg_history : type
+    _vacddg_history : type
         std error in vacuum free energy at equally spaced intervals of simulation (kcal/mol)
-    solddg_history : type
+    _solddg_history : type
         std error in solvent free energy at equally spaced intervals of simulation (kcal/mol)
-    comddg_history : type
+    _comddg_history : type
         std error in complex free energy at equally spaced intervals of simulation (kcal/mol)
     count : int
         Number of times the 'histories' have been 'sampled' from
@@ -74,42 +71,42 @@ class Simulation(object):
 
     """
     from simtk import unit
-    def __init__(self,A,B):
-        self.ligA = A
-        self.ligB = B
-        self.directory = f'lig{self.ligA}to{self.ligB}'
-        self.vacdg = None
-        self.vacddg = None
-        self.soldg = None
-        self.solddg = None
-        self.comdg = None
-        self.comddg = None
 
-        self.vacdg_history = []
-        self.soldg_history = []
-        self.comdg_history = []
-        self.vacddg_history = []
-        self.solddg_history = []
-        self.comddg_history = []
+    def __init__(self, directory):
+        self.directory = directory
+        self._vacdg = None
+        self._vacddg = None
+        self._soldg = None
+        self._solddg = None
+        self._comdg = None
+        self._comddg = None
+        self.hydrationdg = None
+        self.hydrationddg = None
+        self.bindingdg = None
+        self.bindingddg = None
+
+
+        self._vacdg_history = []
+        self._soldg_history = []
+        self._comdg_history = []
+        self._vacddg_history = []
+        self._solddg_history = []
+        self._comddg_history = []
 
         self.count = 0
+
         self._load_data()
 
-        if self.vacdg is not None and self.soldg is not None:
-            self.hydrationdg = self.vacdg - self.soldg
-            self.hydrationddg = (self.vacddg + self.solddg)**0.5
+        if self._vacdg is not None and self._soldg is not None:
+            self.hydrationdg = self._vacdg - self._soldg
+            self.hydrationddg = (self._vacddg**2 + self._solddg**2)**0.5
         else:
             print('Both vacuum and solvent legs need to be run for hydration free energies')
-        if self.comdg is not None and self.soldg is not None:
-            self.bindingdg = self.soldg - self.comdg
-            self.bindingddg = (self.comddg + self.solddg)**0.5
+        if self._comdg is not None and self._soldg is not None:
+            self.bindingdg = self._soldg - self._comdg
+            self.bindingddg = (self._comddg**2 + self._solddg**2)**0.5
         else:
             print('Both solvent and complex legs need to be run for binding free energies')
-
-    @staticmethod
-    def _kt_to_kcal(x):
-        q = unit.quantity.Quantity(x, unit=unit.kilojoules_per_mole)
-        return q / unit.kilocalories_per_mole
 
     def _load_data(self):
         """ Calculate relative free energy details from the simulation by performing MBAR on the vacuum and solvent legs of the simualtion.
@@ -122,44 +119,55 @@ class Simulation(object):
         None
 
         """
-        from pymbar import timeseries
         from pymbar import MBAR
         from perses.analysis import utils
         import os
         from openmmtools.multistate import MultiStateReporter, MultiStateSamplerAnalyzer
+        from simtk import unit
 
         # find the output files
         output = [x for x in os.listdir(self.directory) if x[-3:] == '.nc' and 'checkpoint' not in x]
-
 
         for out in output:
             if 'vacuum' in out:
                 vacuum_reporter = MultiStateReporter(f'{self.directory}/{out}')
                 vacuum_analyzer = MultiStateSamplerAnalyzer(vacuum_reporter)
                 f_ij, df_ij = vacuum_analyzer.get_free_energy()
-                self.vacdg = _kt_to_kcal(f_ij[0, -1])
-                self.vacddg = _kt_to_kcal(df_ij[0, -1] ** 2)
-                self.vacf_ij = f_ij
-                self.vacdf_ij = df_ij
+                f = f_ij[0,-1] * vacuum_analyzer.kT
+                self._vacdg = f.in_units_of(unit.kilocalories_per_mole)
+                df = df_ij[0, -1] * vacuum_analyzer.kT
+                self._vacddg = df.in_units_of(unit.kilocalories_per_mole)
+                self._vacf_ij = f_ij
+                self._vacdf_ij = df_ij
             elif'solvent' in out:
                 solvent_reporter = MultiStateReporter(f'{self.directory}/{out}')
                 solvent_analyzer = MultiStateSamplerAnalyzer(solvent_reporter)
                 f_ij, df_ij = solvent_analyzer.get_free_energy()
-                self.soldg = _kt_to_kcal(f_ij[0, -1])
-                self.solddg = _kt_to_kcal(df_ij[0, -1] ** 2)
-                self.solf_ij = f_ij
-                self.soldf_ij = df_ij
+                f = f_ij[0,-1] * solvent_analyzer.kT
+                self._soldg = f.in_units_of(unit.kilocalories_per_mole)
+                df = df_ij[0, -1] * solvent_analyzer.kT
+                self._solddg = df.in_units_of(unit.kilocalories_per_mole)
+                self._solf_ij = f_ij
+                self._soldf_ij = df_ij
             elif 'complex' in out:
                 complex_reporter = MultiStateReporter(f'{self.directory}/{out}')
                 complex_analyzer = MultiStateSamplerAnalyzer(complex_reporter)
                 f_ij, df_ij = complex_analyzer.get_free_energy()
-                self.comdg = _kt_to_kcal(f_ij[0, -1])
-                self.comddg = _kt_to_kcal(df_ij[0, -1] ** 2)
-                self.comf_ij = f_ij
-                self.comdf_ij = df_ij
+                f = f_ij[0,-1] * complex_analyzer.kT
+                self._comdg = f.in_units_of(unit.kilocalories_per_mole)
+                df = df_ij[0, -1] * complex_analyzer.kT
+                self._comddg = df.in_units_of(unit.kilocalories_per_mole)
+                self._comf_ij = f_ij
+                self._comdf_ij = df_ij
         return
 
+    def report(self):
+        if self.bindingdg is not None:
+            print(f'Relative binding free energy is {self.bindingdg} ({self.bindingddg})')
+        if self.hydrationdg is not None:
+            print(f'Relative binding free energy is {self.hydrationdg} ({self.hydrationddg})')
 
+    # TODO fix this whole thing to be consistent with above
     def historic_fes(self, stepsize=100):
         """ Function that performs mbar at intervals of the simulation
         by postprocessing. Can be slow if stepsize is small
@@ -191,8 +199,8 @@ class Simulation(object):
                 for step in range(stepsize, n_iterations, stepsize):
                     vacuum_analyzer = MultiStateSamplerAnalyzer(vacuum_reporter,max_n_iterations=step)
                     f_ij, df_ij = vacuum_analyzer.get_free_energy()
-                    self.vacdg_history.append(_kt_to_kcal(f_ij[0, -1]))
-                    self.vacddg_history.append(_kt_to_kcal(df_ij[0,-1]))
+                    self._vacdg_history.append(_kt_to_kcal(f_ij[0, -1]))
+                    self._vacddg_history.append(_kt_to_kcal(df_ij[0,-1]))
             if 'solvent' in out:
                 solvent_reporter = MultiStateReporter(f'{self.directory}/{out}')
                 ncfile = utils.open_netcdf(f'{self.directory}/{out}')
@@ -200,8 +208,8 @@ class Simulation(object):
                 for step in range(stepsize, n_iterations, stepsize):
                     solvent_analyzer = MultiStateSamplerAnalyzer(solvent_reporter,max_n_iterations=step)
                     f_ij, df_ij = solvent_analyzer.get_free_energy()
-                    self.soldg_history.append(_kt_to_kcal(f_ij[0, -1]))
-                    self.solddg_history.append(_kt_to_kcal(df_ij[0,-1]))
+                    self._soldg_history.append(_kt_to_kcal(f_ij[0, -1]))
+                    self._solddg_history.append(_kt_to_kcal(df_ij[0,-1]))
             if 'complex' in out:
                 complex_reporter = MultiStateReporter(f'{self.directory}/{out}')
                 ncfile = utils.open_netcdf(f'{self.directory}/{out}')
@@ -209,11 +217,11 @@ class Simulation(object):
                 for step in range(stepsize, n_iterations, stepsize):
                     complex_analyzer = MultiStateSamplerAnalyzer(complex_reporter,max_n_iterations=step)
                     f_ij, df_ij = complex_analyzer.get_free_energy()
-                    self.comdg_history.append(_kt_to_kcal(f_ij[0, -1]))
-                    self.comddg_history.append(_kt_to_kcal(df_ij[0,-1]))
+                    self._comdg_history.append(_kt_to_kcal(f_ij[0, -1]))
+                    self._comddg_history.append(_kt_to_kcal(df_ij[0,-1]))
         return
 
-    def sample_history(self,method='binding'):
+    def sample_history(self, method='binding'):
         """Get the free energy from the historic FE's in steps
 
         Parameters
@@ -227,19 +235,19 @@ class Simulation(object):
             the relative free energy and it's associated std error (kcal/mol)
 
         """
-        vac = self.vacdg_history[self.count]
-        sol = self.soldg_history[self.count]
-        com = self.comdg_history[self.count]
-        vacvar = self.vacddg_history[self.count]
-        solvar = self.solddg_history[self.count]
-        comvar = self.comddg_history[self.count]
+        vac = self._vacdg_history[self.count]
+        sol = self._soldg_history[self.count]
+        com = self._comdg_history[self.count]
+        vacvar = self._vacddg_history[self.count]
+        solvar = self._solddg_history[self.count]
+        comvar = self._comddg_history[self.count]
 
         self.count += 1
 
         if method == 'binding':
-            return sol - com , (solvar**2 + comvar**2)**0.5
+            return sol - com, (solvar**2 + comvar**2)**0.5
         elif method == 'hydration':
-            return  vac - sol , (solvar**2 + vacvar**2)**0.5
+            return vac - sol, (solvar**2 + vacvar**2)**0.5
         else:
             print('method not recognised, choose binding or hydration')
 
