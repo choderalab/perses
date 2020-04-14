f9402d673846b5cf1e84b83a0fe202872ee14b04
Hannah Bruce Macdonald <43652597+hannahbrucemacdonald@users.noreply.github.com>
2020-04-14 17:23:18 +0100

    updating version number

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def find_package_data(data_root, package_root):
 
 
 # #########################
-VERSION = '0.5.1'
+VERSION = '0.6.0'
 ISRELEASED = True 
 __version__ = VERSION
 # #########################
