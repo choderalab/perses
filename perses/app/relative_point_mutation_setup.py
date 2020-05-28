87e8805bb4e6070450c6905e50f1abad9a20125f
Ivy Zhang <ivy.zhang@wustl.edu>
2020-05-28 16:01:57 -0400

    enable apo only option, ligand as protein option, fix formatting

--- perses/app/relative_point_mutation_setup.py
+++ perses/app/relative_point_mutation_setup.py
@@ -1,9 +1,7 @@
 from __future__ import absolute_import
 
-from perses.samplers import multistate
 from perses.utils.openeye import *
 from perses.annihilation.relative import HybridTopologyFactory
-from perses.annihilation.lambda_protocol import LambdaProtocol
 from perses.rjmc.topology_proposal import PointMutationEngine
 from perses.rjmc.geometry import FFAllAngleGeometryEngine
 
@@ -14,8 +12,7 @@ import numpy as np
 from openmoltools import forcefield_generators
 import copy
 import mdtraj as md
-from openmmtools.constants import kB 
-from pkg_resources import resource_filename
+from openmmtools.constants import kB
 from perses.tests.utils import validate_endstate_energies
 from openforcefield.topology import Molecule
 from openmmforcefields.generators import SystemGenerator
@@ -32,26 +29,28 @@ class PointMutationExecutor(object):
     Simple, stripped-down class to create a protein-ligand system and allow a mutation of a protein.
     this will allow support for the creation of _two_ relative free energy calculations:
         1. 'wildtype' - 'point mutant' complex hybrid.
-        2. 'wildtype' - 'point mutant' receptor hybrid (i.e. with ligand of interest unbound)
+        2. 'wildtype' - 'point mutant' protein hybrid (i.e. with ligand of interest unbound)
 
     Example (create full point mutation executor and run parallel tempering on both complex and apo phases):
-        receptor_path = 'data/perses_jacs_systems/thrombin/Thrombin_protein.pdb'
+        from pkg_resources import resource_filename
+
+        protein_path = 'data/perses_jacs_systems/thrombin/Thrombin_protein.pdb'
         ligands_path = 'data/perses_jacs_systems/thrombin/Thrombin_ligands.sdf'
-        receptor_filename = resource_filename('openmmforcefields', receptor_path)
+        protein_filename = resource_filename('openmmforcefields', protein_path)
         ligand_filename = resource_filename('openmmforcefields', ligands_path)
 
-        pm_delivery = PointMutationExecutor(receptor_filename = receptor_filename,
-                                    ligand_filename = ligand_filename,
-                                    mutation_chain_id = '2',
-                                    mutation_residue_id = '198',
-                                     proposed_residue = 'THR',
-                                     phase = 'complex',
-                                     conduct_endstate_validation = False,
-                                     ligand_index = 0,
-                                     forcefield_files = ['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml'],
-                                     barostat = openmm.MonteCarloBarostat(1.0 * unit.atmosphere, temperature, 50),
-                                     forcefield_kwargs = {'removeCMMotion': False, 'ewaldErrorTolerance': 1e-4, 'nonbondedMethod': app.PME, 'constraints' : app.HBonds, 'hydrogenMass' : 4 * unit.amus},
-                                     small_molecule_forcefields = 'gaff-2.11')
+        pm_delivery = PointMutationExecutor(protein_filename=protein_filename,
+                                    mutation_chain_id='2',
+                                    mutation_residue_id='198',
+                                     proposed_residue='THR',
+                                     phase='complex',
+                                     conduct_endstate_validation=False,
+                                     ligand_filename=ligand_filename,
+                                     ligand_index=0,
+                                     forcefield_files=['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml'],
+                                     barostat=openmm.MonteCarloBarostat(1.0 * unit.atmosphere, temperature, 50),
+                                     forcefield_kwargs={'removeCMMotion': False, 'ewaldErrorTolerance': 1e-4, 'nonbondedMethod': app.PME, 'constraints' : app.HBonds, 'hydrogenMass' : 4 * unit.amus},
+                                     small_molecule_forcefields='gaff-2.11')
 
         complex_htf = pm_delivery.get_complex_htf()
         apo_htf = pm_delivery.get_apo_htf()
@@ -76,29 +75,28 @@ class PointMutationExecutor(object):
                                                                                   splitting="V R R R O R R R V",
                                                                                   constraint_tolerance=1e-06),
                                                                                   hybrid_factory=htf, online_analysis_interval=10)
-            hss.setup(n_states=n_states, temperature=300*unit.kelvin, storage_file = reporter, lambda_protocol = lambda_protocol, endstates=False)
+            hss.setup(n_states=n_states, temperature=300*unit.kelvin, storage_file=reporter, lambda_protocol=lambda_protocol, endstates=False)
             hss.extend(n_cycles)
 
     """
     def __init__(self,
-                 receptor_filename,
-                 ligand_filename,
+                 protein_filename,
                  mutation_chain_id,
                  mutation_residue_id,
-                 proposed_residue, phase = 'complex',
-                 conduct_endstate_validation = False,
-                 ligand_index = 0,
-                 forcefield_files = ['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml'],
-                 barostat = openmm.MonteCarloBarostat(1.0 * unit.atmosphere, temperature, 50),
-                 forcefield_kwargs = {'removeCMMotion': False, 'ewaldErrorTolerance': 1e-4, 'nonbondedMethod': app.PME, 'constraints' : app.HBonds, 'hydrogenMass' : 4 * unit.amus},
-                 small_molecule_forcefields = 'gaff-2.11',
+                 proposed_residue,
+                 phase='complex',
+                 conduct_endstate_validation=False,
+                 ligand_filename=None,
+                 ligand_index=0,
+                 forcefield_files=['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml'],
+                 barostat=openmm.MonteCarloBarostat(1.0 * unit.atmosphere, temperature, 50),
+                 forcefield_kwargs={'removeCMMotion': False, 'ewaldErrorTolerance': 1e-4, 'nonbondedMethod': app.PME, 'constraints' : app.HBonds, 'hydrogenMass' : 4 * unit.amus},
+                 small_molecule_forcefields='gaff-2.11',
                  **kwargs):
         """
         arguments
-            receptor_filename : str
-                path to receptor; .pdb
-            ligand_filename : str
-                path to ligand of interest; .sdf or .pdb
+            protein_filename : str
+                path to protein (to mutate); .pdb
             mutation_chain_id : str
                 name of the chain to be mutated
             mutation_residue_id : str
@@ -109,6 +107,8 @@ class PointMutationExecutor(object):
                 if phase == vacuum, then the complex will not be solvated with water; else, it will be solvated with tip3p
             conduct_endstate_validation : bool, default True
                 whether to conduct an endstate validation of the hybrid topology factory
+            ligand_filename : str, default None
+                path to ligand of interest (i.e. small molecule or protein); .sdf or .pdb
             ligand_index : int, default 0
                 which ligand to use
             forcefield_files : list of str, default ['amber14/protein.ff14SB.xml', 'amber14/tip3p.xml']
@@ -120,32 +120,43 @@ class PointMutationExecutor(object):
             small_molecule_forcefields : str, default 'gaff-2.11'
                 the forcefield string for small molecule parametrization
 
-        TODO : allow argument for separate apo structure if it exists separately
-               allow argument for specator ligands besides the 'ligand_filename'
+        TODO : allow argument for spectator ligands besides the 'ligand_filename'
+
         """
 
-        # First thing to do is make a complex and apo...
-        pdbfile = open(receptor_filename, 'r')
-        pdb = app.PDBFile(pdbfile)
-        pdbfile.close()
-        receptor_positions, receptor_topology, receptor_md_topology = pdb.positions, pdb.topology, md.Topology.from_openmm(pdb.topology)
-        receptor_topology = receptor_md_topology.to_openmm()
-        receptor_n_atoms = receptor_md_topology.n_atoms
+        # First thing to do is load the apo protein to mutate...
+        protein_pdbfile = open(protein_filename, 'r')
+        protein_pdb = app.PDBFile(protein_pdbfile)
+        protein_pdbfile.close()
+        protein_positions, protein_topology, protein_md_topology = protein_pdb.positions, protein_pdb.topology, md.Topology.from_openmm(protein_pdb.topology)
+        protein_topology = protein_md_topology.to_openmm()
+        protein_n_atoms = protein_md_topology.n_atoms
 
+        # Load the ligand, if present
         molecules = []
-        ligand_mol = createOEMolFromSDF(ligand_filename, index = ligand_index)
-        ligand_mol = generate_unique_atom_names(ligand_mol)
-        molecules.append(Molecule.from_openeye(ligand_mol,allow_undefined_stereo = False))
-        ligand_positions, ligand_topology = extractPositionsFromOEMol(ligand_mol),  forcefield_generators.generateTopologyFromOEMol(ligand_mol)
-        ligand_md_topology = md.Topology.from_openmm(ligand_topology)
-        ligand_n_atoms = ligand_md_topology.n_atoms
+        if ligand_filename:
+            if ligand_filename.endswith('.sdf'): # small molecule
+                ligand_mol = createOEMolFromSDF(ligand_filename, index=ligand_index)
+                ligand_mol = generate_unique_atom_names(ligand_mol)
+                molecules.append(Molecule.from_openeye(ligand_mol, allow_undefined_stereo=False))
+                ligand_positions, ligand_topology = extractPositionsFromOEMol(ligand_mol),  forcefield_generators.generateTopologyFromOEMol(ligand_mol)
+                ligand_md_topology = md.Topology.from_openmm(ligand_topology)
+                ligand_n_atoms = ligand_md_topology.n_atoms
 
-        # Now create a complex
-        complex_md_topology = receptor_md_topology.join(ligand_md_topology)
-        complex_topology = complex_md_topology.to_openmm()
-        complex_positions = unit.Quantity(np.zeros([receptor_n_atoms + ligand_n_atoms, 3]), unit=unit.nanometers)
-        complex_positions[:receptor_n_atoms, :] = receptor_positions
-        complex_positions[receptor_n_atoms:, :] = ligand_positions
+            elif ligand_filename.endswith('pdb'): # protein
+                ligand_pdbfile = open(ligand_filename, 'r')
+                ligand_pdb = app.PDBFile(ligand_pdbfile)
+                ligand_pdbfile.close()
+                ligand_positions, ligand_topology, ligand_md_topology = ligand_pdb.positions, ligand_pdb.topology, md.Topology.from_openmm(
+                    ligand_pdb.topology)
+                ligand_n_atoms = ligand_md_topology.n_atoms
+
+            # Now create a complex
+            complex_md_topology = protein_md_topology.join(ligand_md_topology)
+            complex_topology = complex_md_topology.to_openmm()
+            complex_positions = unit.Quantity(np.zeros([protein_n_atoms + ligand_n_atoms, 3]), unit=unit.nanometers)
+            complex_positions[:protein_n_atoms, :] = protein_positions
+            complex_positions[protein_n_atoms:, :] = ligand_positions
 
         # Now for a system_generator
         self.system_generator = SystemGenerator(forcefields = forcefield_files,
@@ -155,9 +166,11 @@ class PointMutationExecutor(object):
                                                 molecules=molecules,
                                                 cache=None)
 
-        # Create complex and apo inputs...
-        complex_topology, complex_positions, complex_system = self._solvate(complex_topology, complex_positions, 'tip3p', phase = phase)
-        apo_topology, apo_positions, apo_system = self._solvate(receptor_topology, receptor_positions, 'tip3p', phase = phase)
+        # Solvate apo and complex...
+        apo_input = list(self._solvate(protein_topology, protein_positions, 'tip3p', phase=phase))
+        inputs = [apo_input]
+        if ligand_filename:
+            inputs.append(self._solvate(complex_topology, complex_positions, 'tip3p', phase=phase))
 
         geometry_engine = FFAllAngleGeometryEngine(metadata=None,
                                                 use_sterics=False,
@@ -174,38 +187,38 @@ class PointMutationExecutor(object):
 
         # Run pipeline...
         htfs = []
-        for (top, pos, sys) in zip([complex_topology, apo_topology], [complex_positions, apo_positions], [complex_system, apo_system]):
-            point_mutation_engine = PointMutationEngine(wildtype_topology = top,
-                                                                 system_generator = self.system_generator,
-                                                                 chain_id = mutation_chain_id, # Denote the chain id allowed to mutate (it's always a string variable)
-                                                                 max_point_mutants = 1,
-                                                                 residues_allowed_to_mutate = [mutation_residue_id], # The residue ids allowed to mutate
-                                                                 allowed_mutations = [(mutation_residue_id, proposed_residue)], # The residue ids allowed to mutate with the three-letter code allowed to change
-                                                                 aggregate = True) # Always allow aggregation
+        for (top, pos, sys) in inputs:
+            point_mutation_engine = PointMutationEngine(wildtype_topology=top,
+                                                                 system_generator=self.system_generator,
+                                                                 chain_id=mutation_chain_id, # Denote the chain id allowed to mutate (it's always a string variable)
+                                                                 max_point_mutants=1,
+                                                                 residues_allowed_to_mutate=[mutation_residue_id], # The residue ids allowed to mutate
+                                                                 allowed_mutations=[(mutation_residue_id, proposed_residue)], # The residue ids allowed to mutate with the three-letter code allowed to change
+                                                                 aggregate=True) # Always allow aggregation
 
             topology_proposal = point_mutation_engine.propose(sys, top)
 
             new_positions, logp_proposal = geometry_engine.propose(topology_proposal, pos, beta)
             logp_reverse = geometry_engine.logp_reverse(topology_proposal, new_positions, pos, beta)
 
-            forward_htf = HybridTopologyFactory(topology_proposal = topology_proposal,
-                                                 current_positions =  pos,
-                                                 new_positions = new_positions,
-                                                 use_dispersion_correction = False,
+            forward_htf = HybridTopologyFactory(topology_proposal=topology_proposal,
+                                                 current_positions=pos,
+                                                 new_positions=new_positions,
+                                                 use_dispersion_correction=False,
                                                  functions=None,
-                                                 softcore_alpha = None,
+                                                 softcore_alpha=None,
                                                  bond_softening_constant=1.0,
                                                  angle_softening_constant=1.0,
-                                                 soften_only_new = False,
-                                                 neglected_new_angle_terms = [],
-                                                 neglected_old_angle_terms = [],
-                                                 softcore_LJ_v2 = True,
-                                                 softcore_electrostatics = True,
-                                                 softcore_LJ_v2_alpha = 0.85,
-                                                 softcore_electrostatics_alpha = 0.3,
-                                                 softcore_sigma_Q = 1.0,
-                                                 interpolate_old_and_new_14s = False,
-                                                 omitted_terms = None)
+                                                 soften_only_new=False,
+                                                 neglected_new_angle_terms=[],
+                                                 neglected_old_angle_terms=[],
+                                                 softcore_LJ_v2=True,
+                                                 softcore_electrostatics=True,
+                                                 softcore_LJ_v2_alpha=0.85,
+                                                 softcore_electrostatics_alpha=0.3,
+                                                 softcore_sigma_Q=1.0,
+                                                 interpolate_old_and_new_14s=False,
+                                                 omitted_terms=None)
 
             if not topology_proposal.unique_new_atoms:
                 assert geometry_engine.forward_final_context_reduced_potential == None, f"There are no unique new atoms but the geometry_engine's final context reduced potential is not None (i.e. {self._geometry_engine.forward_final_context_reduced_potential})"
@@ -223,15 +236,14 @@ class PointMutationExecutor(object):
 
 
             if conduct_endstate_validation:
-                zero_state_error, one_state_error = validate_endstate_energies(forward_htf._topology_proposal, forward_htf, added_valence_energy, subtracted_valence_energy, beta = beta, ENERGY_THRESHOLD = ENERGY_THRESHOLD)
+                zero_state_error, one_state_error = validate_endstate_energies(forward_htf._topology_proposal, forward_htf, added_valence_energy, subtracted_valence_energy, beta=beta, ENERGY_THRESHOLD=ENERGY_THRESHOLD)
             else:
                 pass
 
             htfs.append(forward_htf)
 
-
-        self.complex_htf = htfs[0]
-        self.apo_htf = htfs[1]
+        self.apo_htf = htfs[0]
+        self.complex_htf = htfs[1] if ligand_filename else None
 
     def get_complex_htf(self):
         return copy.deepcopy(self.complex_htf)
@@ -269,15 +281,12 @@ class PointMutationExecutor(object):
         solvated_system : openmm.System
             The parameterized system, containing a barostat if one was specified.
         """
-        from pdbfixer import PDBFixer
-        from simtk.openmm.app import PDBFile
-        import os
         modeller = app.Modeller(topology, positions)
 
 
         # Now we have to add missing atoms
         if phase != 'vacuum':
-            modeller.addSolvent(self.system_generator.forcefield, model=model, padding = 1.0 * unit.nanometers, ionicStrength=0.15*unit.molar)
+            modeller.addSolvent(self.system_generator.forcefield, model=model, padding=1.0 * unit.nanometers, ionicStrength=0.15*unit.molar)
         else:
             pass
 
@@ -285,7 +294,7 @@ class PointMutationExecutor(object):
         solvated_positions = modeller.getPositions()
 
         # Canonicalize the solvated positions: turn tuples into np.array
-        solvated_positions = unit.quantity.Quantity(value = np.array([list(atom_pos) for atom_pos in solvated_positions.value_in_unit_system(unit.md_unit_system)]), unit = unit.nanometers)
+        solvated_positions = unit.quantity.Quantity(value=np.array([list(atom_pos) for atom_pos in solvated_positions.value_in_unit_system(unit.md_unit_system)]), unit=unit.nanometers)
         solvated_system = self.system_generator.create_system(solvated_topology)
 
         return solvated_topology, solvated_positions, solvated_system
