54f9996d34db3e69a073afeb62e8479f339e0fc2
William Glass <wglass149@googlemail.com>
2020-06-08 10:30:30 +0100

    use periodic_forcefield_kwargs in syst gen

--- examples/neq-switching/run_equilibrium_setup.py
+++ examples/neq-switching/run_equilibrium_setup.py
@@ -194,7 +194,7 @@ def create_systems(topologies_dict, positions_dict, output_directory, project_pr
     barostat = openmm.MonteCarloBarostat(1.0*unit.atmosphere, temperature, 50)
 
     system_generator = SystemGenerator(['amber14/protein.ff14SB.xml', 'gaff.xml', 'amber14/tip3p.xml', 'MCL1_ligands.xml'], barostat=barostat, forcefield_kwargs={'constraints': app.HBonds,
-    'hydrogenMass': 4 * unit.amus}, nonperiodic_forcefield_kwargs={'nonbondedMethod': app.PME})
+    'hydrogenMass': 4 * unit.amus}, periodic_forcefield_kwargs={'nonbondedMethod': app.PME})
 
     list_of_smiles = list(topologies_dict.keys())
 
