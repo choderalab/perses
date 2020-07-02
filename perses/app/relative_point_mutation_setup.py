f0058a23d131b40d1117022005b277a2f1137149
Ivy Zhang <ivy.zhang@wustl.edu>
2020-07-02 16:13:29 -0400

    change endstate validation asserts to warnings

--- perses/app/relative_point_mutation_setup.py
+++ perses/app/relative_point_mutation_setup.py
@@ -262,8 +262,10 @@ class PointMutationExecutor(object):
 
             if conduct_endstate_validation:
                 zero_state_error, one_state_error = validate_endstate_energies(forward_htf._topology_proposal, forward_htf, added_valence_energy, subtracted_valence_energy, beta=beta, ENERGY_THRESHOLD=ENERGY_THRESHOLD)
-                assert zero_state_error < ENERGY_THRESHOLD, f"Reduced potential difference of the nonalchemical and alchemical Lambda = 0 state is above the threshold ({ENERGY_THRESHOLD}): {zero_state_error}"
-                assert one_state_error < ENERGY_THRESHOLD, f"Reduced potential difference of the nonalchemical and alchemical Lambda = 1 state is above the threshold ({ENERGY_THRESHOLD}): {one_state_error}"
+                if zero_state_error < ENERGY_THRESHOLD:
+                    _logger.warning(f"Reduced potential difference of the nonalchemical and alchemical Lambda = 0 state is above the threshold ({ENERGY_THRESHOLD}): {zero_state_error}")
+                if one_state_error < ENERGY_THRESHOLD:
+                    _logger.warning(f"Reduced potential difference of the nonalchemical and alchemical Lambda = 1 state is above the threshold ({ENERGY_THRESHOLD}): {one_state_error}")
             else:
                 pass
 
