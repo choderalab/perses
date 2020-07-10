a2910baf681ec20d0c0be837a9ee81e2518e1ab0
Hannah Bruce Macdonald <43652597+hannahbrucemacdonald@users.noreply.github.com>
2020-07-10 12:32:14 +0100

    changing v number back to dev after release

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ def find_package_data(data_root, package_root):
 
 
 # #########################
-VERSION = '0.7.2'
-ISRELEASED = True 
+VERSION = '0.7.3dev0'
+ISRELEASED = False
 __version__ = VERSION
 # #########################
 
