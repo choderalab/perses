7a810b61020ecbc3b08f14d865b2893d625b49ec
Ivy Zhang <ivy.zhang@wustl.edu>
2020-07-14 11:46:39 -0400

    set allow_ring_breaking to False if transformation involves TRP

--- perses/rjmc/topology_proposal.py
+++ perses/rjmc/topology_proposal.py
@@ -2176,7 +2176,9 @@ class PolymerProposalEngine(ProposalEngine):
 
         #now we can get the mol atom map of the sidechain
         #NOTE: since the sidechain oemols are NOT zero-indexed anymore, we need to match by name (since they are unique identifiers)
-        local_atom_map_nonstereo_sidechain = AtomMapper._get_mol_atom_map(current_oemol, proposed_oemol, map_strength = 'default', matching_criterion = 'name', map_strategy = 'matching_criterion')
+        ring_bool = False if old_res_name == 'TRP' or new_res_name == 'TRP' else True # Set allow_ring_breaking to be False if the transformation involves TRP
+        _logger.debug(f"\t\t\t allow ring breaking: {ring_bool}")
+        local_atom_map_nonstereo_sidechain = AtomMapper._get_mol_atom_map(current_oemol, proposed_oemol, map_strength = 'default', matching_criterion = 'name', map_strategy = 'matching_criterion', allow_ring_breaking=ring_bool)
 
         #check the atom map thus far:
         _logger.debug(f"\t\t\tlocal atom map nonstereo sidechain: {local_atom_map_nonstereo_sidechain}")
