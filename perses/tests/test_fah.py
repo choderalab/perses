b6ed0863a6105f9baa16c76452f66ec93646d68e
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2020-08-26 15:22:50 +0100

    adding function to test core.xml generation

--- perses/tests/test_fah.py
+++ perses/tests/test_fah.py
@@ -5,3 +5,15 @@ TODO:
 * Write tests
 
 """
+
+
+def test_core_file():
+    """ Checks that a core.xml file is written
+    """
+    import tempfile
+    from perses.app.fah_generator import make_core_file
+    import os
+    tmpdir = tempfile.mkdtemp(prefix='hasfunction-')
+
+    make_core_file(100, 1, 1, directory=tmpdir)
+    assert os.path.exists(f'{tmpdir}/core.xml')
