a95bcb9e2ecaa445c2bedae40beb53865f345d74
Hannah Bruce Macdonald <43652597+hannahbrucemacdonald@users.noreply.github.com>
2020-08-28 15:46:50 +0100

    bumping version

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ def find_package_data(data_root, package_root):
 
 
 # #########################
-VERSION = '0.8.1'
-ISRELEASED = True
+VERSION = '0.8.2dev0'
+ISRELEASED = False
 __version__ = VERSION
 # #########################
 
