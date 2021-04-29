1606b894ff932eac0cd5bd6cea78efc4a36a1519
Ivy Zhang <ivy.zhang@wustl.edu>
2021-04-29 17:33:36 -0400

    fix formatting and get_scale_identifier

--- perses/annihilation/relative.py
+++ perses/annihilation/relative.py
@@ -2547,20 +2547,16 @@ class RxnHybridTopologyFactory(HybridTopologyFactory):
                  topology_proposal,
                  current_positions,
                  new_positions,
-
                  #scaling arguments
                  scale_regions = None,
-
                  #electrostatics
                  electrostatic_nonbonded_expression = None,
                  electrostatic_nonbonded_global_parameters = None,
                  electrostatic_nonbonded_per_particle_parameters = None,
-
                  #sterics
                  steric_nonbonded_expression = None,
                  steric_nonbonded_global_parameters = None,
-                 steric_nonbonded_per_particle_parameters = None
-
+                 steric_nonbonded_per_particle_parameters = None,
                  **kwargs):
         """
         TODO : remove hybrid-indexing from HybridTopologyFactory (this should be given directly to the RxnHybridTopologyFactory via the topology_proposal)
@@ -2597,6 +2593,7 @@ class RxnHybridTopologyFactory(HybridTopologyFactory):
 
 
         """
+        _logger.info("*** Generating RxnHybridTopologyFactory ***")
         _logger.info("Beginning nonbonded method, total particle, barostat, and exceptions retrieval...")
         self._topology_proposal = topology_proposal
         #self._num_alchemical_regions = self._topology_proposal._num_alchemical_regions
@@ -2918,43 +2915,41 @@ class RxnHybridTopologyFactory(HybridTopologyFactory):
         assert type(particles) in [type(set()), int], f"`particles` must be an integer or a set, got {type(particles)}."
         if isinstance(particles, int):
             template = [0 for i in range(self.num_scale_regions + 1)]
-            template[0] = 1
+            template[0] = 1 # by default, the particle belongs to nonscale
             if self.num_scale_regions == 0:
                 return template
             else:
                 for idx, _set in enumerate(self._scale_regions):
-                    if particles.intersection(_set) != {}: #allow for particles that straddle alchemical regions
-                        template[idx+1] += 1 #update the template
-                        template[0] = 0 #remove env default
+                    if particles.intersection(_set) != set(): # allow for particles that straddle alchemical regions
+                        template[idx+1] += 1 # update the template
+                        template[0] = 0 # remove env default
             return template
 
         elif isinstance(particles, set):
             template = [0 for i in range(2 * self.num_scale_regions + 1)]
-            template[0] = 1
-            env_counter = 0 #make an environment counter. add 1 to this every time the particles is not in the intersection with a scale region.
+            template[0] = 1 # by default, the particles belong to nonscale
+            nonscale_counter = 0 # make an nonscale counter. add 1 to this every time the particles is not in the intersection with a scale region.
             if self.num_scale_regions == 0:
                 return template
             else:
                 for idx, _set in enumerate(self._scale_regions):
-                    if particles.intersection(_set) != {}: # at least one of the particles is in the idx_th scale region
-                        #assert that it does not intersect with any other scale region...
+                    if particles.intersection(_set) != set(): # at least one of the particles is in the idx_th scale region
+                        # assert that it does not intersect with any other scale region...
                         further_indices = range(idx+1, self.num_scale_regions)
                         intersections = [particles.intersection(self._scale_regions[q]) for q in further_indices]
                         for region_label, val in zip(further_indices, intersections):
-                            assert val == {}, f"term containing particles {particles} intersects with scale regions {idx} and {region_label}"
-                        if particles.issubset(_set): #then this term is wholly in the scale region
-                            template[2*idx] = 1 #double the index (and add 1) so that we
-                        else: #it is interscale region
-                            template[2*idx+1] = 1
+                            assert val == set(), f"term containing particles {particles} intersects with scale regions {idx} and {region_label}"
+                        if particles.issubset(_set): # then this term is wholly in the scale region
+                            template[2*idx+1] = 1 # double the index (and add 1) so that we
+                        else: # it is interscale region
+                            template[2*idx+2] = 1
                     else:
-                        env_counter += 1
+                        nonscale_counter += 1
 
-                if env_counter == len(self._scale_regions): #then there was no match and it is an env atom
-                    out_template = [1] + template
-                else: #there was a scale match and it is not an env atom.
-                    out_template = [0] + template
+                if nonscale_counter != self.num_scale_regions: # if the particles matched one of the scale regions, set nonscale to 0
+                    template[0] = 0
 
-                return out_template
+                return template
 
         else:
             raise Exception(f"particles is of type {type(particles)}, but only `int` and `set` are allowable")
@@ -2968,7 +2963,7 @@ class RxnHybridTopologyFactory(HybridTopologyFactory):
         template[0] = 1
         str_identifier = 'environment_atom'
         for idx, _set in enumerate(self._alchemical_regions):
-            if particles.intersection(_set) != {}: #allow for particles that straddle alchemical regions
+            if particles.intersection(_set) != set(): #allow for particles that straddle alchemical regions
                 template[idx+1] += 1 #update the template
                 template[0] = 0 #remove env default
 
