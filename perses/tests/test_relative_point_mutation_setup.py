7687a46f3a53144729b9aeae469da8eb109d38f8
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-06-15 10:30:25 -0700

    added pdb file for testing

--- perses/tests/test_relative_point_mutation_setup.py
+++ perses/tests/test_relative_point_mutation_setup.py
@@ -1,6 +1,8 @@
 def test_PointMutationExecutor():
+    from pkg_resources import resource_filename
     from perses.app.relative_point_mutation_setup import PointMutationExecutor
     from simtk import unit
+    pdb_filename = resource_filename('perses', 'ala_vacuum.pdb')
     solvent_delivery = PointMutationExecutor("ala_vacuum.pdb",
                             '1',
                             '2',
