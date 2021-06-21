816c53df59ddbd7cae0a32d4335fbb7af08d774e
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-06-21 15:22:28 -0700

    run black and isort on setup.{cfg,py}

--- setup.py
+++ setup.py
@@ -2,31 +2,33 @@
 """Perses: Tools for expanded-ensemble simulations with OpenMM
 """
 
-from __future__ import print_function, absolute_import
+from __future__ import absolute_import, print_function
 
 DOCLINES = __doc__.split("\n")
 
 import os
 import sys
-import versioneer
-
 from os.path import join as pjoin
 from os.path import relpath
+
 from setuptools import setup
+
+import versioneer
+
 try:
     sys.dont_write_bytecode = True
-    sys.path.insert(0, '.')
+    sys.path.insert(0, ".")
 finally:
     sys.dont_write_bytecode = False
 
 
-if '--debug' in sys.argv:
-    sys.argv.remove('--debug')
+if "--debug" in sys.argv:
+    sys.argv.remove("--debug")
     DEBUG = True
 else:
     DEBUG = False
 
-#Useful function
+# Useful function
 def find_package_data(data_root, package_root):
     files = []
     for root, dirnames, filenames in os.walk(data_root):
@@ -54,24 +56,42 @@ Programming Language :: Python :: 3.8
 
 extensions = []
 
-setup(name='perses',
-      author='Patrick Grinaway',
-      author_email='patrick.grinaway@choderalab.org',
-      description=DOCLINES[0],
-      long_description="\n".join(DOCLINES[2:]),
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      url='https://github.com/choderalab/perses',
-      platforms=['Linux', 'Mac OS-X', 'Unix'],
-      classifiers=CLASSIFIERS.splitlines(),
-      packages=['perses', 'perses.storage', 'perses.analysis', 'perses.samplers', 'perses.rjmc', 'perses.annihilation', 'perses.bias', 'perses.tests', 'perses.dispersed','perses.app','perses.utils'],
-      #package_data={'perses' : find_package_data('perses','examples') + find_package_data('perses','data')}, # I don't think this works
-      package_data={'perses' : find_package_data('perses/data', 'perses')}, # I think this is fixed
-      zip_safe=False,
-      entry_points={
-       'openmm.forcefielddir' : ['perses=perses:get_datadir'],
-       'console_scripts' : ['perses-relative = perses.app.setup_relative_calculation:run','perses-fah = perses.app.fah_generator:run']
-      },
-      ext_modules=extensions,
-      install_requires=[],
-      )
+setup(
+    name="perses",
+    author="Patrick Grinaway",
+    author_email="patrick.grinaway@choderalab.org",
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url="https://github.com/choderalab/perses",
+    platforms=["Linux", "Mac OS-X", "Unix"],
+    classifiers=CLASSIFIERS.splitlines(),
+    packages=[
+        "perses",
+        "perses.storage",
+        "perses.analysis",
+        "perses.samplers",
+        "perses.rjmc",
+        "perses.annihilation",
+        "perses.bias",
+        "perses.tests",
+        "perses.dispersed",
+        "perses.app",
+        "perses.utils",
+    ],
+    # package_data={'perses' : find_package_data('perses','examples') + find_package_data('perses','data')}, # I don't think this works
+    package_data={
+        "perses": find_package_data("perses/data", "perses")
+    },  # I think this is fixed
+    zip_safe=False,
+    entry_points={
+        "openmm.forcefielddir": ["perses=perses:get_datadir"],
+        "console_scripts": [
+            "perses-relative = perses.app.setup_relative_calculation:run",
+            "perses-fah = perses.app.fah_generator:run",
+        ],
+    },
+    ext_modules=extensions,
+    install_requires=[],
+)
