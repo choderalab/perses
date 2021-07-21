680dcad0a963c37d971376255a8fcfda2d76f53d
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-07-21 16:52:18 -0700

    small mol test

--- perses/tests/test_resume.py
+++ perses/tests/test_resume.py
@@ -1,5 +1,81 @@
-def test_resume_small_molecule():
-    pass
+def test_resume_small_molecule(tmp_path):
+    import logging
+    import os
+
+    import simtk.unit as unit
+    from openmmtools import mcmc
+    from openmmtools.multistate import MultiStateReporter
+    from perses.annihilation.lambda_protocol import LambdaProtocol
+    from perses.annihilation.relative import HybridTopologyFactory
+    from perses.app.relative_setup import RelativeFEPSetup
+    from perses.samplers.multistate import HybridRepexSampler
+    from pkg_resources import resource_filename
+
+    smiles_filename = resource_filename("perses", os.path.join("data", "test.smi"))
+    fe_setup = RelativeFEPSetup(
+        ligand_input=smiles_filename,
+        old_ligand_index=0,
+        new_ligand_index=1,
+        forcefield_files=[],
+        small_molecule_forcefield="gaff-2.11",
+        phases=["vacuum"],
+    )
+
+    htf = HybridTopologyFactory(
+        topology_proposal=fe_setup._vacuum_topology_proposal,
+        current_positions=fe_setup._vacuum_positions_old,
+        new_positions=fe_setup._vacuum_positions_new,
+        neglected_new_angle_terms=fe_setup._vacuum_forward_neglected_angles,
+        neglected_old_angle_terms=fe_setup._vacuum_reverse_neglected_angles,
+        softcore_LJ_v2=True,
+        interpolate_old_and_new_14s=False,
+    )
+
+    # Build the hybrid repex samplers
+    _logger = logging.getLogger()
+    _logger.setLevel(logging.DEBUG)
+    selection = "not water"
+    checkpoint_interval = 5
+    n_states = 3
+    n_cycles = 10
+    lambda_protocol = LambdaProtocol(functions="default")
+    reporter_file = tmp_path / "cdk2_solvent.nc"
+    reporter = MultiStateReporter(
+        reporter_file,
+        analysis_particle_indices=htf.hybrid_topology.select(selection),
+        checkpoint_interval=checkpoint_interval,
+    )
+    hss = HybridRepexSampler(
+        mcmc_moves=mcmc.LangevinSplittingDynamicsMove(
+            timestep=4.0 * unit.femtoseconds,
+            collision_rate=5.0 / unit.picosecond,
+            n_steps=250,
+            reassign_velocities=False,
+            n_restart_attempts=20,
+            splitting="V R R R O R R R V",
+            constraint_tolerance=1e-06,
+        ),
+        hybrid_factory=htf,
+        online_analysis_interval=10,
+    )
+    hss.setup(
+        n_states=n_states,
+        temperature=300 * unit.kelvin,
+        storage_file=reporter,
+        lambda_protocol=lambda_protocol,
+    )
+    hss.extend(n_cycles)
+    del hss
+
+    # Load repex simulation
+    reporter = MultiStateReporter(reporter_file, checkpoint_interval=10)
+    simulation = HybridRepexSampler.from_storage(reporter)
+
+    # Resume simulation
+    simulation.extend(5)
+
+    assert simulation.iteration == 15
+
 
 def test_resume_protien_mutation_with_checkpoint(tmp_path):
     import logging
