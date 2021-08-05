dd50eee2e3e4685c17fc7fd82f132418a84f4ff9
Iván <ijpulidos@unal.edu.co>
2021-08-05 12:41:55 -0400

    Small change to find setup script path.

--- examples/SamplerExample/run.py
+++ examples/SamplerExample/run.py
@@ -2,10 +2,11 @@ import yaml
 import sys
 import os
 
-def run_relative_perturbation(ligA, ligB,tidy=True):
-    print('Starting relative calcluation of ligand {} to {}'.format(ligA,ligB))
-    trajectory_directory = 'lig{}to{}'.format(ligA,ligB) 
-    new_yaml = 'cdk2_repex_{}to{}.yaml'.format(ligA,ligB) 
+
+def run_relative_perturbation(ligA, ligB, tidy=True):
+    print('Starting relative calculation of ligand {} to {}'.format(ligA, ligB))
+    trajectory_directory = 'lig{}to{}'.format(ligA, ligB)
+    new_yaml = 'cdk2_repex_{}to{}.yaml'.format(ligA, ligB)
     
     # rewrite yaml file
     with open('cdk2_repex.yaml', "r") as yaml_file:
@@ -18,19 +19,19 @@ def run_relative_perturbation(ligA, ligB,tidy=True):
     
     # run the simulation
     # change this to the path where your perses installation is
-    os.system(f'python $PERSESHOME/perses/app/setup_relative_calculation.py {format(new_yaml)}')
+    setup_script_path = os.path.join('..', '..', 'perses', 'app', 'setup_relative_calculation.py')
+    os.system(f'python {setup_script_path} {new_yaml}')
 
-    print('Relative calcluation of ligand {} to {} complete'.format(ligA,ligB))
+    print('Relative calculation of ligand {} to {} complete'.format(ligA, ligB))
 
     if tidy:
         os.remove(new_yaml)
 
-    return
 
 # work out which ligand pair to run
-#!!! change this to the list of ligand pairs you want to compare
+# !!! change this to the list of ligand pairs you want to compare
 # the first ligand in the .sdf file will be 0 
-ligand_pairs = [(1,10),(2,13),(7,10),(8,10)]
-ligand1, ligand2 = ligand_pairs[int(sys.argv[1])-1] # jobarray starts at 1 
+ligand_pairs = [(1, 10), (2, 13), (7, 10), (8, 10)]
+ligand1, ligand2 = ligand_pairs[int(sys.argv[1])-1]  # 1-based indexing (job array compatibility)
 
 run_relative_perturbation(ligand1, ligand2)
