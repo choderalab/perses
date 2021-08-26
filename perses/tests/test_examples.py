5a2e1cd6a5b67400362669be1975d7541acdb0a8
Iván Pulido <ijpulidos@unal.edu.co>
2021-08-26 17:12:12 -0400

    [WIP] Minimal working, automated and tested examples (#844)
    * Removing example as suggested in #766
    
    * Fixing obvious syntax and import errors selectivity example. WIP.
    
    * Small obvious correction in parameter.
    
    * Small change to find setup script path.
    
    * new switching small style changes
    
    * trying to fix new switching AtomMapper confusion. Still not there. WIP.
    
    * Fixing filename for example.
    
    * Comment on possible unexpected behavior.
    
    * Cleaning up protein mutation neq example.
    
    * No need to have a previous pickled htf.
    
    * Removing obsolete or broken examples.
    
    * Comments on defaults and optional argument for out directory.
    
    * General instruction on how tu run example script. Rewritten.
    
    * Fixing resume test, now using data path for files. Adding data files. Typo for phase.
    
    * Make the example use the pdb data file in the pkg resources. Helps with tests.
    
    * Having CLI args is not a great idea for automated testing. Hardcoding parameters in script.
    
    * Automated tests for examples.
    
    * Better way of catching the output when it fails.
    
    * Decoding output of error when it fails for readability.
    
    * Testing removing conflicting OE_LICENSE variable export.
    
    * changing neq steps to something more stable/reasonable. Avoiding nans.
    
    * protein-protein complex example and automated test.
    
    Actually running the correct Protein mutation.
    
    * Removing the need to copy the OE license file to current directory.
    
    * Adding files for protein-ligand example using the CLI
    
    * Fixing path join.
    
    * fix with undefined variable when reading from sdf.
    
    * Adding example for protein-ligand kinase system.
    
    * Using our own temporary directory context manager.
    
    * dynamically marking slow examples tests.
    
    * Using try-finally for automatic change to temp directory context manager.
    
    * Changing test residue mutation to avoid needing solvent phase. Neutral aa.
    
    * Removing undefined statment. Raising actual error.
    
    * Global imports instead. Removed unused import sys.
    
    * Removing unused variable for tmp dir.
    
    * Removed unused import
    
    Co-authored-by: Mike Henry <11765982+mikemhenry@users.noreply.github.com>

---
+++ perses/tests/test_examples.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# ======================================================================
+# MODULE DOCSTRING
+# ======================================================================
+
+"""
+Test that the examples in the repo run without errors.
+"""
+
+# ======================================================================
+# GLOBAL IMPORTS
+# ======================================================================
+
+import pathlib
+import pytest
+import subprocess
+from perses.tests.utils import enter_temp_directory
+
+ROOT_DIR_PATH = pathlib.Path(__file__).joinpath("../../../").resolve()
+
+
+def run_script_file(file_path, cmd_args=None):
+    """Run through the shell a python script."""
+    with enter_temp_directory():
+        cmd = ["python", file_path]
+        print(cmd)
+        # Extend cmd list with given cmd_args
+        if cmd_args:
+            cmd.extend(cmd_args)
+        try:
+            subprocess.run(cmd, capture_output=True, check=True)
+        except subprocess.CalledProcessError as error:
+            raise Exception(f"Example {file_path} failed. STDERR: {error.stderr.decode()}")
+
+
+def find_example_scripts():
+    """Find all Python scripts, excluding Jupyter notebooks, in the examples folder.
+    Returns
+    -------
+    example_file_paths : List[str]
+        List of full paths to python scripts to execute.
+    """
+    examples_dir_path = ROOT_DIR_PATH.joinpath("examples")
+
+    example_file_paths = []
+    for example_file_path in examples_dir_path.glob("*/*.py"):
+        # TODO: find a better way to mark slow examples
+        example_posix_path = example_file_path.as_posix()
+        if "kinase-neq-switching" in example_posix_path:
+            example_posix_path = pytest.param(example_posix_path, marks=pytest.mark.slow)
+        example_file_paths.append(example_posix_path)
+
+    return example_file_paths
+
+
+# ======================================================================
+# TESTS
+# ======================================================================
+@pytest.mark.parametrize("example_file_path", find_example_scripts())
+def test_examples(example_file_path):
+    """Test that the example run without errors."""
+    run_script_file(example_file_path)
