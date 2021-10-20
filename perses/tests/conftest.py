5ebd91dc6139499bd91057577369f80f47104b50
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-10-20 15:11:35 -0700

    we will use markers with -m to control which get ran

--- perses/tests/conftest.py
+++ perses/tests/conftest.py
@@ -3,26 +3,18 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--runslow", action="store_true", default=False, help="run slow tests"
-    )
-    parser.addoption(
-        "--gpu-ci", action="store_true", default=False, help="run GPU tests on CI"
-    )
+        "--runslow", action="store_true", default=False, help="run slow tests")
 
 
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
-    config.addinivalue_line("markers", "gpu_ci: mark test as useful to run on GPU CI")
-
+    config.addinivalue_line("markers", "gpu_ci: mark test as useful to run on GPU")
+    config.addinivalue_line("markers", "gpu_needed: mark test as GPU required")
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--gpu-ci"):
-        only_GPU = pytest.mark.skip(reason="--gpu-ci used to only run GPU CI tests")
-        for item in items:
-            if not "gpu_ci" in item.keywords:
-                item.add_marker(only_GPU)
+    skip_slow = pytest.mark.skip(reason="need --runslow option to run")
+
     if not config.getoption("--runslow"):
-        skip_slow = pytest.mark.skip(reason="need --runslow option to run")
         for item in items:
             if "slow" in item.keywords:
                 item.add_marker(skip_slow)
