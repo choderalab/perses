c9795db4b7d53851143df8b6b206c7b0db798864
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-11-09 10:29:58 -0700

    fixing issues with fix

--- perses/tests/test_examples.py
+++ perses/tests/test_examples.py
@@ -15,14 +15,14 @@ Test that the examples in the repo run without errors.
 import pathlib
 import pytest
 import subprocess
-from tempfile import TemporaryDirectory
+from perses.tests.utils import enter_temp_directory
 
 ROOT_DIR_PATH = pathlib.Path(__file__).joinpath("../../../").resolve()
 
 
 def run_script_file(file_path, cmd_args=None):
     """Run through the shell a python script."""
-    with enter_temp_directory:
+    with enter_temp_directory():
         cmd = ["python", file_path]
         print(cmd)
         # Extend cmd list with given cmd_args
