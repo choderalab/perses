c9046c78e6230caba5a038ff887014428eb07d71
Iván Pulido <ivanpulido@protonmail.com>
2022-01-24 15:10:12 -0500

    Using utility functions for getting data from URLs.

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 CLI utility to automatically run benchmarks using data from the open force field protein-ligand benchmark at
 https://github.com/openforcefield/protein-ligand-benchmark
@@ -9,13 +11,11 @@ It requires internet connection to function properly, by connecting to the menti
 import argparse
 import logging
 import os
-import random
-import time
-import urllib.request
 import yaml
 
-from http.client import RemoteDisconnected
 from perses.app.setup_relative_calculation import run
+from perses.utils.url_utils import retrieve_file_url
+from perses.utils.url_utils import fetch_url_contents
 
 # Setting logging level config
 LOGLEVEL = os.environ.get("LOGLEVEL", "DEBUG").upper()
@@ -28,50 +28,6 @@ _logger.setLevel(LOGLEVEL)
 
 # global variables
 base_repo_url = "https://github.com/openforcefield/protein-ligand-benchmark"
-
-
-# helper functions
-def retrieve_file_url(url, retries=5):
-    """Retrieves a file from url. Retries if it encounters an undesired exception. """
-    remaining_tries = retries
-
-    while remaining_tries > 0:
-        try:
-            file_path, _ = urllib.request.urlretrieve(url)
-        except (urllib.error.URLError, urllib.error.HTTPError, RemoteDisconnected) as e:
-            wait_time = random.uniform(1, 3)
-            _logger.warning(f"Error downloading {url}. Retrying in {wait_time} seconds...")
-            remaining_tries = remaining_tries - 1
-            last_error = e
-            time.sleep(wait_time)  # wait for a random time between 1-3 seconds
-            continue
-        else:
-            break
-    else:
-        _logger.error(f"Could not fetch data.")
-        raise last_error
-    return file_path
-
-def fetch_url_contents(url, retries=5):
-    """Fetch contents from url. Retries if it encounters an undesired exception. """
-    remaining_tries = retries
-
-    while remaining_tries > 0:
-        try:
-            file_contents = urllib.request.urlopen(url)
-        except (urllib.error.URLError, urllib.error.HTTPError, RemoteDisconnected) as e:
-            wait_time = random.uniform(1, 3)
-            _logger.warning(f"Error downloading {url}. Retrying in {wait_time} seconds...")
-            remaining_tries = remaining_tries - 1
-            last_error = e
-            time.sleep(wait_time)  # wait for a random time between 1-3 seconds
-            continue
-        else:
-            break
-    else:
-        _logger.error(f"Could not fetch data.")
-        raise last_error
-    return file_contents
 
 
 def concatenate_files(input_files, output_file):
