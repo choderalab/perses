36f63f05e0d296d8e053c7c97cd6b6c7fc1e174f
Iván Pulido <ivanpulido@protonmail.com>
2022-01-24 19:52:21 -0500

    Revert "Revert "Resolving merge conflicts.""
    This reverts commit 5c55454b045a7f9b159148b2322374f193d36f25.

--- examples/protein-neq-switching/run_example.py
+++ examples/protein-neq-switching/run_example.py
@@ -1,7 +1,7 @@
 import logging
 import numpy as np
 from openmmtools.integrators import PeriodicNonequilibriumIntegrator
-import openmmtools.utils
+from openmmtools.utils import get_fastest_platform
 from pkg_resources import resource_filename
 from simtk import unit
 from simtk import openmm
@@ -19,6 +19,7 @@ nsteps_eq = 2
 nsteps_neq = 32
 neq_splitting = 'V R H O R V'
 timestep = 4.0 * unit.femtosecond
+platform_name = get_fastest_platform().getName()
 temperature = 300 * unit.kelvin
 save_freq_eq = 1
 save_freq_neq = 2
@@ -62,8 +63,7 @@ integrator = PeriodicNonequilibriumIntegrator(DEFAULT_ALCHEMICAL_FUNCTIONS,
                                               temperature=temperature)
 
 # Set up context
-platform = openmmtools.utils.get_fastest_platform()  # get fastest platform
-platform_name = platform.getName()
+platform = openmm.Platform.getPlatformByName(platform_name)
 if platform_name in ['CUDA', 'OpenCL']:
     platform.setPropertyDefaultValue('Precision', 'mixed')
 if platform_name in ['CUDA']:
