936695386f95b870d7a3b27035280fb3cde9fccc
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-03-10 14:17:34 -0700

    Add some comments to make it more clear what is going on and add next bits

--- perses/app/setup_relative_calculation.py
+++ perses/app/setup_relative_calculation.py
@@ -878,6 +878,7 @@ def run(yaml_filename=None):
                     else:
                         endstates = [0] if setup_options['direction'] == 'forward' else [1]
                     #ne_fep_run.activate_client(LSF = LSF, processes = 2, adapt = adapt) #we only need 2 processes for equilibration
+                    # Minimize
                     retry_attempt = 0
                     MAX_ATTEMPTS = 5
                     while retry_attempt < MAX_ATTEMPTS:
@@ -897,14 +898,52 @@ def run(yaml_filename=None):
                             f"Will try one last time and not catch the exception"
                         )
                         ne_fep_run.minimize_sampler_states()
-                    # MMH here
-                    ne_fep_run.equilibrate(n_equilibration_iterations = setup_options['n_equilibration_iterations'],
-                                           n_steps_per_equilibration = setup_options['n_equilibrium_steps_per_iteration'],
-                                           endstates = [0,1],
-                                           max_size = setup_options['max_file_size'],
-                                           decorrelate = True,
-                                           timer = True,
-                                           minimize = False)
+                    # Equilibrate
+                    retry_attempt = 0
+                    MAX_ATTEMPTS = 5
+                    while retry_attempt < MAX_ATTEMPTS:
+                        try:
+                            ne_fep_run.equilibrate(
+                                n_equilibration_iterations=setup_options[
+                                    "n_equilibration_iterations"
+                                ],
+                                n_steps_per_equilibration=setup_options[
+                                    "n_equilibrium_steps_per_iteration"
+                                ],
+                                endstates=[0, 1],
+                                max_size=setup_options["max_file_size"],
+                                decorrelate=True,
+                                timer=True,
+                                minimize=False,
+                            )
+
+                        except OpenMMException as err:
+                            _logger.error(f"OpenMMException! {err}")
+                            retry_attempt += 1
+                            _logger.error(
+                                f"retry attempt {retry_attempt}/{MAX_ATTEMPTS}"
+                            )
+                    else:
+                        _logger.error(
+                            f"Failed to retry simulation in {MAX_ATTEMPTS} attempts"
+                        )
+                        _logger.error(
+                            f"Will try one last time and not catch the exception"
+                        )
+                        ne_fep_run.equilibrate(
+                            n_equilibration_iterations=setup_options[
+                                "n_equilibration_iterations"
+                            ],
+                            n_steps_per_equilibration=setup_options[
+                                "n_equilibrium_steps_per_iteration"
+                            ],
+                            endstates=[0, 1],
+                            max_size=setup_options["max_file_size"],
+                            decorrelate=True,
+                            timer=True,
+                            minimize=False,
+                        )
+
                     #ne_fep_run.deactivate_client()
                     with open(os.path.join(trajectory_directory, "%s_%s_fep.eq.pkl" % (trajectory_prefix, phase)), 'wb') as f:
                         pickle.dump(ne_fep_run, f)
