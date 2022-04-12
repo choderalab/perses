1a9cb29a215965a99ee7dc3c6fa1cd3b2a012366
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-04-12 15:43:42 -0700

    print debug info and make script more robust

--- examples/new-cli/run_star_map.sh
+++ examples/new-cli/run_star_map.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-center=0
+set -xeuo pipefail
+
+old_ligand_idx=0
 for new_ligand_idx in $(seq 1 5)
 do 
-	echo perses-cli --yaml my.yaml --override old_ligand_index:"$center" --override new_ligand_index:"$new_ligand_idx" --override trajectory_directory:lig"$center"to"$new_ligand_idx"
+	perses-cli --yaml my.yaml --override old_ligand_index:"$old_ligand_idx" --override new_ligand_index:"$new_ligand_idx" --override trajectory_directory:lig"$old_ligand_idx"to"$new_ligand_idx"
 done
