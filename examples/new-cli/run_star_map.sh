7db505fa59381c50060bc67e08a8bfff96a1a6f1
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2022-04-15 09:00:23 -0700

    use all the ligands in the sdf

--- examples/new-cli/run_star_map.sh
+++ examples/new-cli/run_star_map.sh
@@ -3,7 +3,7 @@
 set -xeuo pipefail
 
 old_ligand_idx=0
-for new_ligand_idx in $(seq 1 5)
+for new_ligand_idx in $(seq 1 11)
 do 
 	perses-cli --yaml my.yaml --override old_ligand_index:"$old_ligand_idx" --override new_ligand_index:"$new_ligand_idx" --override trajectory_directory:lig"$old_ligand_idx"to"$new_ligand_idx"
 done
