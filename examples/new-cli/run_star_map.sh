43a266d21d5c4f066d729d61bf997b0b2d295d5e
John Chodera <john.chodera@choderalab.org>
2022-05-08 03:21:39 -0400

    Updates to CLI example: * updated docked ligands with better alignment * updated run_star_map.sh script * new LSF file to run dense map * rename my.yaml -> template.yaml

--- examples/new-cli/run_star_map.sh
+++ examples/new-cli/run_star_map.sh
@@ -5,5 +5,5 @@ set -xeuo pipefail
 old_ligand_idx=0
 for new_ligand_idx in $(seq 1 10)
 do 
-	perses-cli --yaml my.yaml --override old_ligand_index:"$old_ligand_idx" --override new_ligand_index:"$new_ligand_idx" --override trajectory_directory:lig"$old_ligand_idx"to"$new_ligand_idx"
+    perses-cli --yaml template.yaml --override old_ligand_index:"$old_ligand_idx" --override new_ligand_index:"$new_ligand_idx" --override trajectory_directory:lig"$old_ligand_idx"to"$new_ligand_idx"
 done
---
+++ examples/new-cli/submit-dense-map.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#BSUB -P "perses-ripk2"
+#BSUB -J "perses-benchmark-rip2-5ns-[1-182]"
+#BSUB -n 1
+#BSUB -R rusage[mem=8]
+#BSUB -R span[hosts=1]
+#BSUB -q gpuqueue
+#BSUB -sp 1 # low priority. default is 12, max is 25
+#BSUB -gpu num=1:j_exclusive=yes:mode=shared
+#BSUB -W 14:00
+##BSUB -m "lu-gpu lg-gpu lv-gpu ld-gpu lt-gpu lp-gpu ls-gpu lx-gpu ly-gpu ll-gpu boson"
+## Excluding ls-gpu lu-gpu and lg-gpu nodes
+#BSUB -m "lu-gpu ld-gpu lx-gpu ly-gpu"
+#BSUB -o out_%J_%I.stdout
+#BSUB -eo out_%J_%I.stderr
+#BSUB -L /bin/bash
+
+source ~/.bashrc
+OPENMM_CPU_THREADS=1
+
+echo "changing directory to ${LS_SUBCWD}"
+cd $LS_SUBCWD
+conda activate perses-dev
+
+# Report node in use
+hostname
+
+# Open eye license activation/env
+export OE_LICENSE=~/.openeye/oe_license.txt
+
+# Report CUDA info
+env | sort | grep 'CUDA'
+
+# Report GPU info
+nvidia-smi -L
+nvidia-smi --query-gpu=name --format=csv
+
+NLIGANDS=14
+old_ligand_index=$(( LSB_JOBINDEX / NLIGANDS ))
+new_ligand_index=$(( LSB_JOBINDEX % NLIGANDS ))
+if (( $old_ligand_index < $new_ligand_index )); then
+    perses-cli --yaml template.yaml --override old_ligand_index:${old_ligand_index} --override new_ligand_index:${new_ligand_index} --override trajectory_directory:long_run_lig${old_ligand_index}to${new_ligand_index}
+fi
+
