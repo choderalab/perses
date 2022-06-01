7a05c884df5934cd7086ae04a6d415836eeb66d2
Ivy Zhang <35546250+zhang-ivy@users.noreply.github.com>
2022-06-01 13:32:03 -0400

    Speed up functions for retrieving old/new positions from hybrid positions (#1020)
    * speed up
    
    * remove unnecessary quantity and update docstrings
    
    Co-authored-by: Mike Henry <11765982+mikemhenry@users.noreply.github.com>
    Co-authored-by: Iván Pulido <ivanpulido@protonmail.com>

--- perses/annihilation/relative.py
+++ perses/annihilation/relative.py
@@ -2158,20 +2158,19 @@ class HybridTopologyFactory(object):
         Parameters
         ----------
         hybrid_positions : [n, 3] np.ndarray or simtk.unit.Quantity
-            The positions of the hybrid system
+            The positions of the hybrid system (in nanometers)
 
         Returns
         -------
         old_positions : [m, 3] np.ndarray with unit
-            The positions of the old system
+            The positions of the old system (in nanometers)
         """
         n_atoms_old = self._topology_proposal.n_atoms_old
         # making sure hybrid positions are simtk.unit.Quantity objects
         if not isinstance(hybrid_positions, unit.Quantity):
             hybrid_positions = unit.Quantity(hybrid_positions, unit=unit.nanometer)
-        old_positions = unit.Quantity(np.zeros([n_atoms_old, 3]), unit=unit.nanometer)
-        for idx in range(n_atoms_old):
-            old_positions[idx, :] = hybrid_positions[idx, :]
+        hybrid_indices = [self._old_to_hybrid_map[idx] for idx in range(n_atoms_old)]
+        old_positions = hybrid_positions[hybrid_indices, :]
         return old_positions
 
     def new_positions(self, hybrid_positions):
@@ -2181,20 +2180,19 @@ class HybridTopologyFactory(object):
         Parameters
         ----------
         hybrid_positions : [n, 3] np.ndarray or simtk.unit.Quantity
-            The positions of the hybrid system
+            The positions of the hybrid system (in nanometers)
 
         Returns
         -------
         new_positions : [m, 3] np.ndarray with unit
-            The positions of the new system
+            The positions of the new system (in nanometers)
         """
         n_atoms_new = self._topology_proposal.n_atoms_new
         # making sure hybrid positions are simtk.unit.Quantity objects
         if not isinstance(hybrid_positions, unit.Quantity):
             hybrid_positions = unit.Quantity(hybrid_positions, unit=unit.nanometer)
-        new_positions = unit.Quantity(np.zeros([n_atoms_new, 3]), unit=unit.nanometer)
-        for idx in range(n_atoms_new):
-            new_positions[idx, :] = hybrid_positions[self._new_to_hybrid_map[idx], :]
+        hybrid_indices = [self._new_to_hybrid_map[idx] for idx in range(n_atoms_new)]
+        new_positions = hybrid_positions[hybrid_indices, :]
         return new_positions
 
     @property
