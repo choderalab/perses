1be02445a57f5c7266fab439bd0903a462b5941b
zhang-ivy <ivy.zhang@wustl.edu>
2022-06-17 10:48:32 -0400

    pass argument to RelativeFEPSetup

--- perses/app/setup_relative_calculation.py
+++ perses/app/setup_relative_calculation.py
@@ -522,7 +522,8 @@ def run_setup(setup_options, serialize_systems=True, build_samplers=True):
                                           small_molecule_forcefield=setup_options['small_molecule_forcefield'], small_molecule_parameters_cache=setup_options['small_molecule_parameters_cache'],
                                           trajectory_directory=trajectory_directory, trajectory_prefix=setup_options['trajectory_prefix'], nonbonded_method=setup_options['nonbonded_method'],
                                           complex_box_dimensions=setup_options['complex_box_dimensions'],solvent_box_dimensions=setup_options['solvent_box_dimensions'], ionic_strength=ionic_strength, remove_constraints=setup_options['remove_constraints'],
-                                          use_given_geometries=use_given_geometries, given_geometries_tolerance=given_geometries_tolerance)
+                                          use_given_geometries=use_given_geometries, given_geometries_tolerance=given_geometries_tolerance,
+                                          transform_waters_into_ions_for_charge_changes = setup_options['transform_waters_into_ions_for_charge_changes'])
 
 
         _logger.info(f"\twriting pickle output...")
