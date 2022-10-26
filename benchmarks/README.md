570d80ac913f5701f0f0d52e853ca302fd0f2c1c
Iván Pulido <ijpulidos@user.noreply.github.com>
2022-10-26 16:09:47 -0400

    Improving script for running benchmarks. Adding local and branch/revision capabilities. README updated.

--- benchmarks/README.md
+++ benchmarks/README.md
@@ -5,7 +5,7 @@ This subdirectory exposes a CLI tool for running automated benchmarks from
 
 ## Running benchmarks
 
-Assuming you have a clone of the perses code repository and you are standing in the `benchmarks` subdirectory
+Assuming you have a clone of the perses code repository, and you are standing in the `benchmarks` subdirectory
 (where this file lives). Then the benchmarks can be run using the following command syntax:
 ```bash
 python run_benchmarks.py --target [protein-name] --edge [edge-index]
@@ -22,6 +22,23 @@ Should the calculation for an edge fail, you can simply re-run the same command-
 # Resume failed edge 6
 python run_benchmarks.py --target tyk2 --edge 6
 ```
+
+### Running benchmark with local files
+The script accepts the `--local` flag to run benchmarks using the files already in the same directory as the 
+`run_benchmarks.py` script. It requires the following files and names in the same directory: `template.yaml`, 
+`ligands.sdf` and `target.pdb`. And you can run using local files with:
+```bash
+python run_benchmarks.py --target tyk2 --edge 6 --local
+```
+
+### Specifying a git revision/tag
+Since the upstream repository is prone to changes. We can specify a revision or a tag using the `--revision` argument.
+For example if we want to use the 0.2.0 tag release, we can run with:
+```bash
+python run_benchmarks.py --target tyk2 --edge 6 --revision=0.2.0
+```
+By default, it uses `0.2.1` as the revision to date. You can also specify branches of the repository such as `main`.
+
 For more information on how to use the tool, you can run `python run_benchmarks.py -h`.
 
 ## Analyzing benchmarks
@@ -35,6 +52,7 @@ For example, for tyk2 results:
 ```bash
 python benchmark_analysis.py --target tyk2
 ```
-This will generate an output CSV file for [`arsenic`](https://github.com/openforcefield/arsenic) and corresponding absolute and relative free energy plots as PNG files produced according to best practices.)
+This will generate an output CSV file for [`arsenic`](https://github.com/openforcefield/arsenic) and corresponding 
+absolute and relative free energy plots as PNG files produced according to best practices.)
 
 For more information on how to use the cli analysis tool use `python benchmark_analysis.py -h`.
--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -28,19 +28,98 @@ _logger.setLevel(LOGLEVEL)
 
 # global variables
 base_repo_url = "https://github.com/openforcefield/protein-ligand-benchmark"
-# TODO: Hardcoding branch (plan of making it an cli argument in the future)
-branch = "fix_data"
 
 
 def concatenate_files(input_files, output_file):
     """
     Concatenate files given in input_files iterator into output_file.
     """
+    input_files = list(input_files)  # handles both single or multiple files
     with open(output_file, 'w') as outfile:
         for filename in input_files:
             with open(filename) as infile:
                 for line in infile:
                     outfile.write(line)
+
+
+def get_target_dir(target_name):
+    """
+    Retrieves the target subdirectory in upstream repo structure given the target name.
+    """
+    # Targets information on branch
+    targets_url = f"{base_repo_url}/raw/{branch}/data/targets.yml"
+    with fetch_url_contents(targets_url) as response:
+        targets_dict = yaml.safe_load(response.read())
+    target_dir = targets_dict[target]['dir']
+    return target_dir
+
+
+def get_ligands_information(target, branch="main"):
+    """
+    Retrieves the ligands information in a dictionary given the target name,
+    """
+    # TODO: This part should be done using plbenchmarks API - once there is a conda pkg
+    target_dir = get_target_dir(target)
+    ligands_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/00_data/ligands.yml"
+    with fetch_url_contents(ligands_url) as response:
+        ligands_dict = yaml.safe_load(response.read())
+    return ligands_dict
+
+
+def generate_ligands_sdf(ligands_dict, target, branch='main'):
+    """
+    Generates input ligands.sdf file for running the simulation given a dictionary
+    with the ligands information.
+
+    Can handle previous and new structure of upstream repo to date (26-Oct-2022).
+    """
+    from urllib.error import HTTPError
+    # Get target dir where to get ligands sdf files
+    target_dir = get_target_dir(target)
+    # Fetch ligands sdf files and concatenate them in one
+    # TODO: This part should be done using plbenchmarks API - once there is a conda pkg
+    try:
+        ligands_file_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/02_ligands/ligands.sdf"
+        ligand_files = retrieve_file_url(ligands_file_url)
+        new_repo = True
+    except HTTPError:
+        _logger.info(f'Must be previous old repository structure. Trying downloading individual ligand files.')
+        ligand_files = []
+        for ligand in ligands_dict.keys():
+            ligand_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/02_ligands/{ligand}/crd/{ligand}.sdf"
+            ligand_file = retrieve_file_url(ligand_url)
+            ligand_files.append(ligand_file)
+        new_repo = False
+    # concatenate sdf files as needed
+    if new_repo:
+        # TODO: Just retrieve the ligands file to the working directory without need to concatenate
+        concatenate_files((ligand_files,), "ligands.sdf")  # "concatenate" single file
+    else:
+        concatenate_files(ligand_files, 'ligands.sdf')  # concatenate multiple files
+
+def get_ligand_indices_from_edge(edge_index, target_name, branch="main"):
+    """
+    Returns the ligand indices from the edge index according to edges.yaml file in repo.
+
+    Allows specifying a branch/revision in repo.
+    """
+    # fetch edges information
+    # TODO: This part should be done using plbenchmarks API - once there is a conda pkg
+    target_dir = get_target_dir(target_name)
+    ligands_dict = get_ligands_information(target, branch=branch)
+    edges_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/00_data/edges.yml"
+    with fetch_url_contents(edges_url) as response:
+        edges_dict = yaml.safe_load(response.read())
+    edges_list = list(edges_dict.values())  # suscriptable edges object - note dicts are ordered for py>=3.7
+
+    edge = edges_list[edge_index]
+    ligand_a_name = edge['ligand_a']
+    ligand_b_name = edge['ligand_b']
+    # ligands list to get indices -- preserving same order as upstream yaml file
+    ligands_list = list(ligands_dict.keys())
+    lig_a_index = ligands_list.index(ligand_a_name)
+    lig_b_index = ligands_list.index(ligand_b_name)
+    return lig_a_index, lig_b_index
 
 
 def run_relative_perturbation(lig_a_idx, lig_b_idx, reverse=False, tidy=True):
@@ -102,7 +181,7 @@ def run_relative_perturbation(lig_a_idx, lig_b_idx, reverse=False, tidy=True):
 # Defining command line arguments
 # fetching targets from github repo
 # TODO: This part should be done using plbenchmarks API - once there is a conda pkg
-targets_url = f"{base_repo_url}/raw/{branch}/data/targets.yml"
+targets_url = f"{base_repo_url}/raw/main/data/targets.yml"
 with fetch_url_contents(targets_url) as response:
     targets_dict = yaml.safe_load(response.read())
 # get the possible choices from targets yaml file
@@ -127,54 +206,55 @@ arg_parser.add_argument(
     action='store_true',
     help="Whether to run the edge in reverse direction. Helpful for consistency checks."
 )
+arg_parser.add_argument(
+    "--revision",
+    type=str,
+    default="0.2.1",
+    help="Specify revision, release or branch in upstream repo to use. Defaults to using the 0.2.1 release branch."
+)
+arg_parser.add_argument(
+    "--local",
+    action='store_true',
+    help="Run simulation with local data. It expects template.yaml, "
+         "ligands.sdf and target.pdb in the same directory as this script."
+)
+arg_parser.add_argument(
+    "--ligand-a",
+
+)
 args = arg_parser.parse_args()
 target = args.target
+edge_index = args.edge
 is_reversed = args.reversed
+branch = args.revision
+local_run = args.local
 
-# Fetch protein pdb file
-# TODO: This part should be done using plbenchmarks API - once there is a conda pkg
-target_dir = targets_dict[target]['dir']
-pdb_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/01_protein/crd/protein.pdb"
-pdb_file = retrieve_file_url(pdb_url)
+if local_run:
+    get_ligand_indices_from_edge(edge_index, target, branch=branch)
+    lig_a_index, lig_b_index = get_ligand_indices_from_edge(edge_index, target, branch=branch)
+    run_relative_perturbation(lig_a_index, lig_b_index, reverse=is_reversed)
+else:
+    # get target information
+    target_dir = get_target_dir(target)
+    pdb_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/01_protein/crd/protein.pdb"
+    pdb_file = retrieve_file_url(pdb_url)
 
-# Fetch cofactors crystalwater pdb file
-# TODO: This part should be done using plbenchmarks API - once there is a conda pkg
-# TODO: No cofactors now in fix_data branch
-#cofactors_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/01_protein/crd/cofactors_crystalwater.pdb"
-#cofactors_file = retrieve_file_url(cofactors_url)
+    # Fetch cofactors crystalwater pdb file
+    # TODO: This part should be done using plbenchmarks API - once there is a conda pkg
+    # TODO: No cofactors now in fix_data branch
+    #cofactors_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/01_protein/crd/cofactors_crystalwater.pdb"
+    #cofactors_file = retrieve_file_url(cofactors_url)
 
-# Concatenate protein with cofactors pdbs
-# TODO: No need to concatenate now that there are no cofactors
-concatenate_files((pdb_file,), 'target.pdb')
+    # Concatenate protein with cofactors pdbs
+    # TODO: No need to concatenate now that there are no cofactors
+    concatenate_files((pdb_file,), 'target.pdb')
 
-# Fetch ligands sdf files and concatenate them in one
-# TODO: This part should be done using plbenchmarks API - once there is a conda pkg
-ligands_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/00_data/ligands.yml"
-with fetch_url_contents(ligands_url) as response:
-    ligands_dict = yaml.safe_load(response.read())
-ligand_files = []
-for ligand in ligands_dict.keys():
-    ligand_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/02_ligands/{ligand}/crd/{ligand}.sdf"
-    ligand_file = retrieve_file_url(ligand_url)
-    ligand_files.append(ligand_file)
-# concatenate sdfs
-concatenate_files(ligand_files, 'ligands.sdf')
+    # Fetch ligands sdf files and concatenate them in one
+    ligands_dict = get_ligands_information(target, branch=branch)
+    generate_ligands_sdf(ligands_dict, target, branch=branch)
 
-# run simulation
-# fetch edges information
-# TODO: This part should be done using plbenchmarks API - once there is a conda pkg
-edges_url = f"{base_repo_url}/raw/{branch}/data/{target_dir}/00_data/edges.yml"
-with fetch_url_contents(edges_url) as response:
-    edges_dict = yaml.safe_load(response.read())
-edges_list = list(edges_dict.values())  # suscriptable edges object - note dicts are ordered for py>=3.7
-# edge list to access by index
-edge_index = args.edge  # read from cli arguments
-edge = edges_list[edge_index]
-ligand_a_name = edge['ligand_a']
-ligand_b_name = edge['ligand_b']
-# ligands list to get indices -- preserving same order as upstream yaml file
-ligands_list = list(ligands_dict.keys())
-lig_a_index = ligands_list.index(ligand_a_name)
-lig_b_index = ligands_list.index(ligand_b_name)
-# Perform the simulation
-run_relative_perturbation(lig_a_index, lig_b_index, reverse=is_reversed)
+    # get ligand indices
+    lig_a_index, lig_b_index = get_ligand_indices_from_edge(edge_index, target, branch=branch)
+
+    # run simulation
+    run_relative_perturbation(lig_a_index, lig_b_index, reverse=is_reversed)
