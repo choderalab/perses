b7ce14c8e81881ef07feb11a5705291e1a353a0a
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2023-01-30 12:59:54 -0500

    Short minimal test working. Failure tests still WIP.

--- perses/tests/protocols/conftest.py
+++ perses/tests/protocols/conftest.py
@@ -1,1 +1,107 @@
 # fixtures for chemicalcomponents and chemicalsystems to test protocols with
+import gufe
+import pytest
+import importlib.resources
+from rdkit import Chem
+from gufe.mapping import LigandAtomMapping
+
+
+@pytest.fixture
+def benzene_modifications():
+    with importlib.resources.path('gufe.tests.data',
+                                  'benzene_modifications.sdf') as f:
+        supp = Chem.SDMolSupplier(str(f), removeHs=False)
+
+        mols = list(supp)
+
+    return {m.GetProp('_Name'): m for m in mols}
+
+
+# Components fixtures
+
+@pytest.fixture
+def solvent_comp():
+    yield gufe.SolventComponent(positive_ion="Na", negative_ion="Cl")
+
+@pytest.fixture
+def benzene(benzene_modifications):
+    return gufe.SmallMoleculeComponent(benzene_modifications["benzene"])
+
+
+@pytest.fixture
+def toluene(benzene_modifications):
+    return gufe.SmallMoleculeComponent(benzene_modifications["toluene"])
+
+
+# Systems fixtures
+
+@pytest.fixture
+def benzene_vacuum_system(benzene):
+    return gufe.ChemicalSystem(
+        {"ligand": benzene}
+    )
+
+@pytest.fixture
+def benzene_solvent_system(benzene, solvent_comp):
+    return gufe.ChemicalSystem(
+        {"ligand": benzene, "solvent": solvent_comp}
+    )
+
+@pytest.fixture
+def toluene_vacuum_system(toluene):
+    return gufe.ChemicalSystem(
+        {"ligand": toluene}
+    )
+
+@pytest.fixture
+def toluene_solvent_system(toluene, solvent_comp):
+    return gufe.ChemicalSystem(
+        {"ligand": toluene, "solvent": solvent_comp}
+    )
+
+
+# Settings fixtures
+
+@pytest.fixture
+def short_settings():
+    from openff.units import unit
+    # Build Settings gufe object
+    from gufe.settings.models import (
+        Settings,
+    )
+    from perses.protocols.settings import NonEqCyclingSettings
+
+    settings = Settings.get_defaults()
+    settings.thermo_settings.temperature = 300 * unit.kelvin
+    settings.protocol_settings = NonEqCyclingSettings(eq_steps=25000, neq_steps=25000, save_frequency=200)
+
+    return settings
+
+
+@pytest.fixture
+def production_settings(short_settings):
+    from perses.protocols.settings import NonEqCyclingSettings
+    settings = short_settings
+    settings.protocol_settings = NonEqCyclingSettings(eq_steps=250000, neq_steps=250000, save_frequency=2000)
+
+    return settings
+
+
+# Mappings fixtures
+
+@pytest.fixture
+def mapping_benzene_toluene(benzene, toluene):
+    """Mapping from toluene to benzene"""
+    mapping_toluene_to_benzene = {4: 0, 5: 1, 6: 2, 7: 3, 8: 4, 9: 5, 10: 6, 11: 7, 12: 8, 13: 9, 14: 11}
+    mapping_obj = LigandAtomMapping(
+        componentA=benzene,
+        componentB=toluene,
+        componentA_to_componentB=mapping_toluene_to_benzene,
+    )
+    return mapping_obj
+
+
+@pytest.fixture
+def broken_mapping(benzene, toluene):
+    """Broken mapping"""
+    ...
--- perses/tests/protocols/test_nonequilibrium_cycling.py
+++ perses/tests/protocols/test_nonequilibrium_cycling.py
@@ -1,8 +1,97 @@
 import pytest
+from perses.protocols import NonEquilibriumCyclingProtocol
+from gufe.protocols.protocoldag import ProtocolDAGResult, execute_DAG
+from gufe.protocols.protocolunit import ProtocolUnitResult
+from gufe.protocols.protocolunit import ProtocolUnitFailure
 
 
-class TestNonEquilibriumCyclcing:
-    ...
+class TestNonEquilibriumCycling:
 
-    def test_create(self):
-        ...
+    @pytest.fixture
+    def protocol_short(self, short_settings):
+        return NonEquilibriumCyclingProtocol(settings=short_settings)
+
+    @pytest.fixture
+    def protocol_dag(self, protocol_short, benzene_vacuum_system, toluene_vacuum_system, mapping_benzene_toluene):
+        dag = protocol_short.create(
+            stateA=benzene_vacuum_system, stateB=toluene_vacuum_system, name="Short vacuum transformation",
+            mapping=mapping_benzene_toluene
+        )
+        dagresult: ProtocolDAGResult = execute_DAG(dag)
+
+        return protocol_short, dag, dagresult
+
+    def test_dag_execute(self, protocol_dag):
+        protocol, dag, dagresult = protocol_dag
+
+        assert dagresult.ok()
+
+        # the FinishUnit will always be the last to execute
+        finishresult = dagresult.protocol_unit_results[-1]
+        assert finishresult.name == "result"
+
+    def test_terminal_units(self, protocol_dag):
+        prot, dag, res = protocol_dag
+
+        finals = res.terminal_protocol_unit_results
+
+        assert len(finals) == 1
+        assert isinstance(finals[0], ProtocolUnitResult)
+        assert finals[0].name == 'result'
+
+    def test_dag_execute_failure(self, protocol_dag_broken):
+        # TODO: Lets create a mapping that doesn't make sense for the ligand transformation. This should raise an error
+        protocol, dag, dagfailure = protocol_dag_broken
+
+        assert not dagfailure.ok()
+        assert isinstance(dagfailure, ProtocolDAGResult)
+
+        failed_units = dagfailure.protocol_unit_failures
+
+        assert len(failed_units) == 1
+        assert failed_units[0].name == "problem child"
+
+        # parse exception arguments
+        assert failed_units[0].exception[1][1]['data'] == "lol"
+        assert isinstance(failed_units[0], ProtocolUnitFailure)
+
+        succeeded_units = dagfailure.protocol_unit_results
+
+        assert len(succeeded_units) > 0
+
+    def test_dag_execute_failure_raise_error(self, protocol_short, benzene_vacuum_system, toluene_vacuum_system,
+                                             broken_mapping):
+        """Executes a bad setup of a protocol DAG which has an incorrect mapping"""
+        dag = protocol_short.create(
+            stateA=benzene_vacuum_system, stateB=toluene_vacuum_system, name="a broken dummy run",
+            mapping=broken_mapping
+        )
+
+        with pytest.raises(AssertionError):
+            execute_DAG(dag, raise_error=True)
+
+    def test_create_execute_gather(self, protocol_short, benzene_vacuum_system, toluene_vacuum_system,
+                                   mapping_benzene_toluene):
+        """
+        Perform 20 independent simulations of the NEQ cycling protocol and gather the results
+        """
+
+        n_simulations = 20
+        results = []
+        for i in range(n_simulations):
+            protocol_dag = protocol_short.create(
+                stateA=benzene_vacuum_system,
+                stateB=toluene_vacuum_system,
+                mapping=mapping_benzene_toluene,
+                name=f"{i}"
+            )
+            protocol_dag_result = execute_DAG(protocol_dag)
+            results.append(protocol_dag_result)
+        # gather aggregated results of interest
+        protocolresult = protocol_short.gather(results)
+
+        # TODO: use the convergence criteria we've been using in perses (DDG < 6*dDDG)
+        assert len(protocolresult.data['logs']) == 1
+        assert len(protocolresult.data['logs'][0]) == 21 + 1
+
+        assert protocolresult.get_estimate() == 105336
